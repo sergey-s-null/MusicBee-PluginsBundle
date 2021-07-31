f384fb4ed8daa4870c1ea036848178406b84398a
Laiser399 <Laiser399@mail.ru>
2021-07-31 13:22:31 +0300

    [Minor] тест настроек в Test Desktop

--- Test Desktop/App.xaml.cs
+++ Test Desktop/App.xaml.cs
@@ -1,9 +1,17 @@
-﻿namespace Test_Desktop
+﻿using System.Windows;
+using MusicBeePlugin.GUI.SettingsDialog;
+
+namespace Test_Desktop
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var wnd = new SettingsDialog();
+            wnd.ShowDialog();
+        }
     }
 }
