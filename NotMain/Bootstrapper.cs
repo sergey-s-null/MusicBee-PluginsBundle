8429f52e33a81a1cbf65387be265cfdd22c8c629
Laiser399 <Laiser399@mail.ru>
2021-07-31 11:41:52 +0300

    изменил проект Main как WPF проект

---
+++ NotMain/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Ninject;
+using Root;
+using VkMusicDownloader;
+
+namespace MusicBeePlugin
+{
+    public static class Bootstrapper
+    {
+        public static IReadOnlyKernel GetKernel(MusicBeeApiInterface mbApi)
+        {
+            var kernel = new KernelConfiguration(new MusicDownloaderModule(mbApi));
+
+            
+            
+
+            return kernel.BuildReadonlyKernel();
+        }
+    }
+}
