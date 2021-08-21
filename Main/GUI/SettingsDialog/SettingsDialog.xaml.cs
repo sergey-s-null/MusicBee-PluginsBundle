6c154040f8a6f74013ab89ac814b670ee3d2bd92
Laiser399 <Laiser399@mail.ru>
2021-08-21 09:31:56 +0300

    сохранение и загрузка

--- Main/GUI/SettingsDialog/SettingsDialog.xaml.cs
+++ Main/GUI/SettingsDialog/SettingsDialog.xaml.cs
@@ -4,11 +4,26 @@ namespace MusicBeePlugin.GUI.SettingsDialog
 {
     public partial class SettingsDialog : Window
     {
+        private readonly SettingsDialogVM _viewModel;
+        
         public SettingsDialog(SettingsDialogVM viewModel)
         {
             InitializeComponent();
 
+            _viewModel = viewModel;
             DataContext = viewModel;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (_viewModel.Save())
+            {
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Error save settings.", "Error");
+            }
         }
     }
 }
--- Main/GUI/SettingsDialog/SettingsDialogVM.cs
+++ Main/GUI/SettingsDialog/SettingsDialogVM.cs
@@ -1,28 +1,76 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using Module.ArtworksSearcher.GUI.Settings;
 using Module.VkMusicDownloader.GUI.Settings;
+using Root.Abstractions;
+using Root.MVVM;
 
 namespace MusicBeePlugin.GUI.SettingsDialog
 {
-    public class SettingsDialogVM : INotifyPropertyChanged
+    public class SettingsDialogVM : ISettings, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public bool IsLoaded => Settings.All(s => s.ModuleSettings.IsLoaded);
+        
         public ObservableCollection<ModuleSettingsVM> Settings { get; } = new ();
 
         public ModuleSettingsVM SelectedSettingsModule { get; set; }
         
-        public SettingsDialogVM(IMusicDownloaderSettingsVM musicDownloaderSettingsVM,
+        private RelayCommand _resetCmd;
+        public RelayCommand ResetCmd
+            => _resetCmd ??= new RelayCommand(_ => Reset());
+        
+        public SettingsDialogVM(
+            IMusicDownloaderSettingsVM musicDownloaderSettingsVM,
             IArtworksSearcherSettingsVM artworksSearcherSettingsVM)
         {
             Settings.Add(new ModuleSettingsVM("Music downloader", 
                 musicDownloaderSettingsVM));
             Settings.Add(new ModuleSettingsVM("Artworks searcher", 
                 artworksSearcherSettingsVM));
+
+            SelectFirstSettingsModule();
             
-            musicDownloaderSettingsVM.Load();// TODO сделать это более универсализированным. Мб через ISettings
-            artworksSearcherSettingsVM.Load();
+            Load();
+        }
+
+        private void SelectFirstSettingsModule()
+        {
+            if (Settings.Count > 0)
+            {
+                SelectedSettingsModule = Settings.First();
+            }
+        }
+        
+        public void Load()
+        {
+            foreach (var setting in Settings)
+            {
+                setting.ModuleSettings.Load();
+            }
+        }
+
+        public bool Save()
+        {
+            foreach (var setting in Settings)
+            {
+                if (!setting.ModuleSettings.Save())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            foreach (var setting in Settings)
+            {
+                setting.ModuleSettings.Reset();
+            }
         }
     }
 }
