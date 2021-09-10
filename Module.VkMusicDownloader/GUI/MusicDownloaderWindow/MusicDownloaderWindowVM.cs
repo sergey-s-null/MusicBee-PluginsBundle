413be47571d2ee445519873fee43127d52470a62
Laiser399 <Laiser399@mail.ru>
2021-09-10 21:28:58 +0300

    Удален AddingIncoming

--- Module.VkMusicDownloader/GUI/MusicDownloaderWindow/MusicDownloaderWindowVM.cs
+++ Module.VkMusicDownloader/GUI/MusicDownloaderWindow/MusicDownloaderWindowVM.cs
@@ -1,19 +1,15 @@
-﻿using Module.VkMusicDownloader.GUI.MusicDownloaderWindow.AddingIncoming;
-using Module.VkMusicDownloader.GUI.MusicDownloaderWindow.AddingVk;
+﻿using Module.VkMusicDownloader.GUI.MusicDownloaderWindow.AddingVk;
 
 namespace Module.VkMusicDownloader.GUI.MusicDownloaderWindow
 {
     public class MusicDownloaderWindowVM
     {
         public AddingVkVM AddingVkVM { get; }
-        public AddingIncomingVM AddingIncomingVM { get; }
         
         public MusicDownloaderWindowVM(
-            AddingVkVM addingVkVM,
-            AddingIncomingVM addingIncomingVM)
+            AddingVkVM addingVkVM)
         {
             AddingVkVM = addingVkVM;
-            AddingIncomingVM = addingIncomingVM;
         }
     }
 }
