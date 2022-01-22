d5583b708a834615307c4723177db8ff7f620ae2
Laiser399 <Laiser399@mail.ru>
2022-01-22 13:40:41 +0300

    диалог выбора файла по индексу

---
+++ Module.InboxAdder/GUI/FileByIndexSelectDialog/FileByIndexSelectDialog.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+
+namespace Module.InboxAdder.GUI.FileByIndexSelectDialog
+{
+    public partial class FileByIndexSelectDialog : Window
+    {
+        private readonly FileByIndexSelectDialogVM _viewModel;
+        
+        public FileByIndexSelectDialog(FileByIndexSelectDialogVM viewModel)
+        {
+            _viewModel = viewModel;
+            
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        public bool ShowDialog(out string filePath)
+        {
+            if (ShowDialog() != true || _viewModel.FilePath is null)
+            {
+                filePath = string.Empty;
+                return false;
+            }
+            
+            filePath = _viewModel.FilePath;
+            return true;
+        }
+        
+        private void OnOkClick(object sender, RoutedEventArgs e)
+        {
+            if (_viewModel.FilePath is null)
+            {
+                MessageBox.Show("Nothing selected.", "Error");
+                return;
+            }
+            
+            DialogResult = true;
+        }
+        
+        private void OnCancelClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
---
+++ Module.InboxAdder/GUI/FileByIndexSelectDialog/FileByIndexSelectDialogVM.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PropertyChanged;
+using Root;
+using Root.Helpers;
+
+namespace Module.InboxAdder.GUI.FileByIndexSelectDialog
+{
+    [AddINotifyPropertyChangedInterface]
+    public class FileByIndexSelectDialogVM
+    {
+        private readonly MusicBeeApiInterface _mbApi;
+        private readonly IReadOnlyDictionary<int, string> _allAudionByIndex;
+
+        [OnChangedMethod(nameof(OnIndexChanged))]
+        public string Index { get; set; } = "";
+        public string FileName { get; private set; } = "";
+        public string Directory { get; private set; } = "";
+
+        [DoNotNotify]
+        public string? FilePath { get; private set; }
+
+        public FileByIndexSelectDialogVM(MusicBeeApiInterface mbApi)
+        {
+            _mbApi = mbApi;
+            _allAudionByIndex = GetAllFilesByIndices();
+        }
+
+        private IReadOnlyDictionary<int, string> GetAllFilesByIndices()
+        {
+            _mbApi.Library_QueryFilesEx("", out var files);
+            
+            var filesByIndices = new Dictionary<int, string>();
+            
+            foreach (var file in files)
+            {
+                if (_mbApi.TryGetIndex(file, out var index) 
+                    && !filesByIndices.ContainsKey(index))
+                {
+                    filesByIndices.Add(index, file);
+                }
+            }
+
+            return filesByIndices;
+        }
+
+        private void OnIndexChanged()
+        {
+            if (int.TryParse(Index, out var index) && _allAudionByIndex.ContainsKey(index))
+            {
+                var filePath = _allAudionByIndex[index];
+                FileName = Path.GetFileName(filePath);
+                Directory = Path.GetDirectoryName(filePath) ?? string.Empty;
+                FilePath = filePath;
+            }
+            else
+            {
+                FileName = "";
+                Directory = "";
+                FilePath = null;
+            }
+        }
+    }
+}
