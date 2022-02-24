fded1bd944350551c3c57429380cd6972346c4c0
Sergestus Shimonov <Laiser399@mail.ru>
2022-02-24 21:27:36 +0300

    rename project MBApiProtoGenerator -> CodeGenerator

--- CodeGenerator/Properties/AssemblyInfo.cs
+++ CodeGenerator/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MBApiProtoGenerator")]
+[assembly: AssemblyTitle("CodeGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MBApiProtoGenerator")]
+[assembly: AssemblyProduct("CodeGenerator")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
