c4a0c9bea3cfef1a1f4391a974b8f4f106b48ebc
Sergestus Shimonov <Laiser399@mail.ru>
2022-03-04 21:12:17 +0300

    убрал события после сборки (сделал копирование dll ручным)

---
+++ copy_dll_debug.cmd
@@ -0,0 +1,2 @@
+copy /Y ".\Plugin.Main\bin\Debug\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
+copy /Y ".\Plugin.RemoteApi\bin\Debug\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
---
+++ copy_dll_release.cmd
@@ -0,0 +1,2 @@
+copy /Y ".\Plugin.Main\bin\Release\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
+copy /Y ".\Plugin.RemoteApi\bin\Release\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
