1d661d888e68e6e86cf2cd3a74c797fd99e68547
Sergey Semenov <Laiser399@mail.ru>
2022-06-01 10:48:59 +0300

    Comparer module native test project

---
+++ NativeTest.AudioSourcesComparer/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Module.AudioSourcesComparer.GUI.Factories;
+using Ninject;
+
+namespace NativeTest.AudioSourcesComparer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var kernel = Bootstrapper.GetKernel();
+
+            var window = kernel
+                .Get<IVkToLocalComparerWindowFactory>()
+                .Create();
+
+            window.ShowDialog();
+        }
+    }
+}
---
+++ NativeTest.AudioSourcesComparer/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Module.AudioSourcesComparer;
+using Module.AudioSourcesComparer.GUI.AbstractViewModels;
+using Module.AudioSourcesComparer.GUI.DesignTimeViewModels;
+using Ninject;
+
+namespace NativeTest.AudioSourcesComparer
+{
+    public static class Bootstrapper
+    {
+        public static IKernel GetKernel()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Load<AudioSourcesComparerModule>();
+
+            kernel
+                .Rebind<IVkToLocalComparerWindowVM>()
+                .To<VkToLocalComparerWindowDTVM>();
+
+            return kernel;
+        }
+    }
+}
