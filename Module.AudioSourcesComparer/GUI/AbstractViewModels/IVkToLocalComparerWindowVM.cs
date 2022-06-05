29aa89a07ad8c4bd0e341d9c97e621235262e489
Sergey Semenov <Laiser399@mail.ru>
2022-06-05 23:35:19 +0300

    remove left/right hide buttons

--- Module.AudioSourcesComparer/GUI/AbstractViewModels/IVkToLocalComparerWindowVM.cs
+++ Module.AudioSourcesComparer/GUI/AbstractViewModels/IVkToLocalComparerWindowVM.cs
@@ -6,10 +6,10 @@ namespace Module.AudioSourcesComparer.GUI.AbstractViewModels
     public interface IVkToLocalComparerWindowVM
     {
         ICommand RefreshCmd { get; }
-        
+
         IList<IVkAudioVM> VkOnlyAudios { get; }
         ICommand DeleteAllVkOnlyAudiosCmd { get; }
-        
+
         IList<IMBAudioVM> LocalOnlyAudios { get; }
     }
 }
