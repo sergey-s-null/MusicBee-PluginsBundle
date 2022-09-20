4409f6026b7d8ce9c91f65f6b8c0746ea502543a
Sergey Semenov <Laiser399@mail.ru>
2022-09-21 00:34:06 +0300

    Remove useless cmd scripts (copy dll)

--- copy_dll_debug.cmd
+++
@@ -1,2 +0,0 @@
-copy /Y ".\Plugin.Main\bin\Debug\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
-copy /Y ".\Plugin.RemoteApi\bin\Debug\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
--- copy_dll_release.cmd
+++
@@ -1,2 +0,0 @@
-copy /Y ".\Plugin.Main\bin\Release\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
-copy /Y ".\Plugin.RemoteApi\bin\Release\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
