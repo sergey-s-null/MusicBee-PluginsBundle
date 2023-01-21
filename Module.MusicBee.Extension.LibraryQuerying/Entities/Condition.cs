a52400633d1343c0796d22a8a08954fde99977fd
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 23:05:11 +0300

    Add predefined conditions

---
+++ Module.MusicBee.Extension.LibraryQuerying/Entities/Condition.cs
@@ -0,0 +1,18 @@
+﻿using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+using Module.MusicBee.Extension.LibraryQuerying.Extensions;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Entities;
+
+public static class Condition
+{
+    public static readonly BaseCondition Empty = Field.None.StartsWith(string.Empty);
+
+    public static readonly BaseCondition HasLyrics = Field.HasLyrics.IsTrue();
+    public static readonly BaseCondition HasNotLyrics = Field.HasLyrics.IsFalse();
+
+    public static readonly BaseCondition AlbumPlayed = Field.AlbumPlayed.IsTrue();
+    public static readonly BaseCondition AlbumNotPlayed = Field.AlbumPlayed.IsFalse();
+    
+    public static readonly BaseCondition AlbumComplete = Field.AlbumComplete.IsTrue();
+    public static readonly BaseCondition AlbumNotComplete = Field.AlbumComplete.IsFalse();
+}
