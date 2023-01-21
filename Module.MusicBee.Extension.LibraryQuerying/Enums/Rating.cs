b538c4a44c24b84313a6b8af4aa4f7ec89a08bc2
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 21:01:53 +0300

    Add Rating and empty RatingFieldExtensions

---
+++ Module.MusicBee.Extension.LibraryQuerying/Enums/Rating.cs
@@ -0,0 +1,24 @@
+﻿namespace Module.MusicBee.Extension.LibraryQuerying.Enums;
+
+public sealed class Rating
+{
+    public static readonly Rating Unset = new(-1);
+
+    public static readonly Rating Half = new(10);
+    public static readonly Rating One = new(20);
+    public static readonly Rating OneAndHalf = new(30);
+    public static readonly Rating Two = new(40);
+    public static readonly Rating TwoAndHalf = new(50);
+    public static readonly Rating Three = new(60);
+    public static readonly Rating ThreeAndHalf = new(70);
+    public static readonly Rating Four = new(80);
+    public static readonly Rating FourAndHalf = new(90);
+    public static readonly Rating Five = new(100);
+
+    public int Value { get; }
+
+    private Rating(int value)
+    {
+        Value = value;
+    }
+}
---
+++ Module.MusicBee.Extension.LibraryQuerying/Extensions/RatingFieldExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+using Module.MusicBee.Extension.LibraryQuerying.Enums;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Extensions;
+
+public static class RatingFieldExtensions
+{
+    /// <summary>
+    /// Равно
+    /// </summary>
+    public static BaseCondition Is(this IRatingField field, Rating rating)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не равно
+    /// </summary>
+    public static BaseCondition IsNot(this IRatingField field, Rating rating)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Больше, чем
+    /// </summary>
+    public static BaseCondition GreaterThan(this IRatingField field, Rating rating)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Меньше, чем
+    /// </summary>
+    public static BaseCondition LessThan(this IRatingField field, Rating rating)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// В диапазоне
+    /// </summary>
+    public static BaseCondition InRange(this IRatingField field, Rating from, Rating to)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не в диапазоне
+    /// </summary>
+    public static BaseCondition NotInRange(this IRatingField field, Rating from, Rating to)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не пустое значение
+    /// </summary>
+    public static BaseCondition IsNotNull(this IRatingField field)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Пустое значение
+    /// </summary>
+    public static BaseCondition IsNull(this IRatingField field)
+    {
+        throw new NotImplementedException();
+    }
+}
