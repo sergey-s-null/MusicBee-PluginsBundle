f351dbfaf0d9847c88284526d3d444501860b3a8
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 19:55:56 +0300

    Add DateFieldExtensions, TimeUnit

---
+++ Module.MusicBee.Extension.LibraryQuerying/Enums/TimeUnit.cs
@@ -0,0 +1,16 @@
+﻿namespace Module.MusicBee.Extension.LibraryQuerying.Enums;
+
+public sealed class TimeUnit
+{
+    public static readonly TimeUnit Minutes = new("h");
+    public static readonly TimeUnit Days = new("d");
+    public static readonly TimeUnit Weeks = new("w");
+    public static readonly TimeUnit Months = new("m");
+
+    public string Postfix { get; }
+
+    private TimeUnit(string postfix)
+    {
+        Postfix = postfix;
+    }
+}
---
+++ Module.MusicBee.Extension.LibraryQuerying/Extensions/DateFieldExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+using Module.MusicBee.Extension.LibraryQuerying.Enums;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Extensions;
+
+public static class DateFieldExtensions
+{
+    /// <summary>
+    /// Равно
+    /// </summary>
+    public static BaseCondition Is(this IDateField field, DateTime dateTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не равно
+    /// </summary>
+    public static BaseCondition IsNot(this IDateField field, DateTime dateTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// После
+    /// </summary>
+    public static BaseCondition GreaterThan(this IDateField field, DateTime dateTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// До
+    /// </summary>
+    public static BaseCondition LessThan(this IDateField field, DateTime dateTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// За последние
+    /// </summary>
+    public static BaseCondition InTheLast(this IDateField field, int time, TimeUnit timeUnit)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не за последние
+    /// </summary>
+    public static BaseCondition NotInTheLast(this IDateField field, int time, TimeUnit timeUnit)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не пустое значение
+    /// </summary>
+    public static BaseCondition IsNotNull(this IDateField field)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Пустое значение
+    /// </summary>
+    public static BaseCondition IsNull(this IDateField field)
+    {
+        throw new NotImplementedException();
+    }
+}
