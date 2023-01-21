b48ec500677962b6d6f117621d3d78f7c5d4cdf0
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 16:44:22 +0300

    Add StringField

---
+++ Module.MusicBee.Extension.LibraryQuerying/Entities/StringField.cs
@@ -0,0 +1,13 @@
+﻿using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Entities;
+
+public sealed class StringField : IStringField
+{
+    public string Name { get; }
+
+    public StringField(string name)
+    {
+        Name = name;
+    }
+}
