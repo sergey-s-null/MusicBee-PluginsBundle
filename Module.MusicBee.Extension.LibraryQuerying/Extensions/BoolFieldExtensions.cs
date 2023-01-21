699458113d9bb43f5293f2f421347a835c2d6afd
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 20:50:03 +0300

    Add BoolFieldExtensions

---
+++ Module.MusicBee.Extension.LibraryQuerying/Extensions/BoolFieldExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Module.MusicBee.Extension.LibraryQuerying.Entities;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Extensions;
+
+public static class BoolFieldExtensions
+{
+    public static BaseCondition IsTrue(this IBoolField field)
+    {
+        return new ConditionWithoutValue(field, Comparison.IsNotNull);
+    }
+
+    public static BaseCondition IsFalse(this IBoolField field)
+    {
+        return new ConditionWithoutValue(field, Comparison.IsNull);
+    }
+}
