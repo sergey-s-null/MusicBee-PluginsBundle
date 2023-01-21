182525ca22f5e12ba7808ead842fd29069e7009e
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 12:20:26 +0300

    Remove dependency from MusicBeeModule

--- Module.MusicBee/MusicBeeModule.cs
+++ Module.MusicBee/MusicBeeModule.cs
@@ -6,22 +6,11 @@ namespace Module.MusicBee;
 
 public class MusicBeeModule : Autofac.Module
 {
-    private readonly MusicBeeApiMemoryContainer _musicBeeApiMemoryContainer;
-
-    public MusicBeeModule(MusicBeeApiMemoryContainer musicBeeApiMemoryContainer)
-    {
-        _musicBeeApiMemoryContainer = musicBeeApiMemoryContainer;
-    }
-
     protected override void Load(ContainerBuilder builder)
     {
         builder
             .RegisterType<MusicBeeApiMemoryContainerWrapper>()
             .As<IMusicBeeApi>()
-            .SingleInstance();
-        builder
-            .Register(_ => _musicBeeApiMemoryContainer)
-            .AsSelf()
             .SingleInstance();
     }
 }
