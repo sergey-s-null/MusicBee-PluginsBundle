71052e3a455455598e0bb81b4931dcfb865cd66d
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 20:44:42 +0300

    Add TimeOffset

---
+++ Module.MusicBee.Extension.LibraryQuerying/Entities/TimeOffset.cs
@@ -0,0 +1,15 @@
+﻿using Module.MusicBee.Extension.LibraryQuerying.Enums;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Entities;
+
+internal sealed class TimeOffset
+{
+    public int Time { get; }
+    public TimeUnit TimeUnit { get; }
+
+    public TimeOffset(int time, TimeUnit timeUnit)
+    {
+        Time = time;
+        TimeUnit = timeUnit;
+    }
+}
