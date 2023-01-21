edce5f0b4c495a43ba8603ff73146b2942818bc8
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 12:16:37 +0300

    Add MusicBeeModule

---
+++ Module.MusicBee/MusicBeeModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Module.MusicBee.Services;
+using Module.MusicBee.Services.Abstract;
+
+namespace Module.MusicBee;
+
+public class MusicBeeModule : Autofac.Module
+{
+    private readonly MusicBeeApiMemoryContainer _musicBeeApiMemoryContainer;
+
+    public MusicBeeModule(MusicBeeApiMemoryContainer musicBeeApiMemoryContainer)
+    {
+        _musicBeeApiMemoryContainer = musicBeeApiMemoryContainer;
+    }
+
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<MusicBeeApiMemoryContainerWrapper>()
+            .As<IMusicBeeApi>()
+            .SingleInstance();
+        builder
+            .Register(_ => _musicBeeApiMemoryContainer)
+            .AsSelf()
+            .SingleInstance();
+    }
+}
