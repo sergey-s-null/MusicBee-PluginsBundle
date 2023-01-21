2f95fd63e090ed3b1ab4c4a6b34680865886a1f6
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 16:24:53 +0300

    Add BaseCondition.cs

---
+++ Module.MusicBee.Extension.LibraryQuerying/Entities/Abstract/BaseCondition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+public abstract class BaseCondition
+{
+    public abstract XNode Build();
+
+    public static BaseCondition operator &(BaseCondition first, BaseCondition second)
+    {
+        throw new NotImplementedException();
+    }
+}
