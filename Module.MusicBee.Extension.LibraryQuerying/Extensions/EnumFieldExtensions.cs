fa34477172bb3c8cdf7a5941df66f7fa023b0e9a
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 16:55:56 +0300

    Add EnumFieldExtensions; Move methods from StringFieldExtensions to EnumFieldExtensions

---
+++ Module.MusicBee.Extension.LibraryQuerying/Extensions/EnumFieldExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Module.MusicBee.Extension.LibraryQuerying.Entities;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Extensions;
+
+public static class EnumFieldExtensions
+{
+    /// <summary>
+    /// Равно
+    /// </summary>
+    public static BaseCondition Is(this IEnumField field, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не равно
+    /// </summary>
+    public static BaseCondition IsNot(this IEnumField field, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Любое из
+    /// </summary>
+    public static BaseCondition IsIn(this IEnumField field, params string[] values)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Любое из
+    /// </summary>
+    public static BaseCondition IsIn(this IEnumField field, IEnumerable<string> values)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Ни одно из
+    /// </summary>
+    public static BaseCondition IsNotIn(this IEnumField field, params string[] values)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Ни одно из
+    /// </summary>
+    public static BaseCondition IsNotIn(this IEnumField field, IEnumerable<string> values)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не пустое значение
+    /// </summary>
+    public static BaseCondition IsNotNull(this IEnumField field)
+    {
+        return new ConditionWithoutValue(field, Comparison.IsNotNull);
+    }
+
+    /// <summary>
+    /// Пустое значение
+    /// </summary>
+    public static BaseCondition IsNull(this IEnumField field)
+    {
+        return new ConditionWithoutValue(field, Comparison.IsNull);
+    }
+
+    /// <summary>
+    /// Содержит
+    /// </summary>
+    public static BaseCondition Contains(this IEnumField field, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не содержит
+    /// </summary>
+    public static BaseCondition DoesNotContain(this IEnumField field, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// В иерархии тегов
+    /// </summary>
+    public static BaseCondition InTagHierarchy(this IEnumField field, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Удовлетворяет RegEx
+    /// </summary>
+    public static BaseCondition MatchesRegEx(this IEnumField field, string value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Удовлетворяет RegEx /i
+    /// </summary>
+    public static BaseCondition MatchesRegExIgnoreCase(this IEnumField field, string value)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Module.MusicBee.Extension.LibraryQuerying/Extensions/StringFieldExtensions.cs
+++ Module.MusicBee.Extension.LibraryQuerying/Extensions/StringFieldExtensions.cs
@@ -1,76 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using Module.MusicBee.Extension.LibraryQuerying.Entities;
 using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
 
 namespace Module.MusicBee.Extension.LibraryQuerying.Extensions;
 
 public static class StringFieldExtensions
 {
-    /// <summary>
-    /// Равно
-    /// </summary>
-    public static BaseCondition Is(this IStringField field, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Не равно
-    /// </summary>
-    public static BaseCondition IsNot(this IStringField field, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Любое из
-    /// </summary>
-    public static BaseCondition IsIn(this IStringField field, params string[] values)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Любое из
-    /// </summary>
-    public static BaseCondition IsIn(this IStringField field, IEnumerable<string> values)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Ни одно из
-    /// </summary>
-    public static BaseCondition IsNotIn(this IStringField field, params string[] values)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Ни одно из
-    /// </summary>
-    public static BaseCondition IsNotIn(this IStringField field, IEnumerable<string> values)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Не пустое значение
-    /// </summary>
-    public static BaseCondition IsNotNull(this IStringField field)
-    {
-        return new ConditionWithoutValue(field, Comparison.IsNotNull);
-    }
-
-    /// <summary>
-    /// Пустое значение
-    /// </summary>
-    public static BaseCondition IsNull(this IStringField field)
-    {
-        return new ConditionWithoutValue(field, Comparison.IsNull);
-    }
-
     /// <summary>
     /// Начинается с
     /// </summary>
@@ -83,46 +17,6 @@ public static class StringFieldExtensions
     /// Заканчивается
     /// </summary>
     public static BaseCondition EndsWith(this IStringField field, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Содержит
-    /// </summary>
-    public static BaseCondition Contains(this IStringField field, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Не содержит
-    /// </summary>
-    public static BaseCondition DoesNotContain(this IStringField field, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// В иерархии тегов
-    /// </summary>
-    public static BaseCondition InTagHierarchy(this IStringField field, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Удовлетворяет RegEx
-    /// </summary>
-    public static BaseCondition MatchesRegEx(this IStringField field, string value)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// Удовлетворяет RegEx /i
-    /// </summary>
-    public static BaseCondition MatchesRegExIgnoreCase(this IStringField field, string value)
     {
         throw new NotImplementedException();
     }
