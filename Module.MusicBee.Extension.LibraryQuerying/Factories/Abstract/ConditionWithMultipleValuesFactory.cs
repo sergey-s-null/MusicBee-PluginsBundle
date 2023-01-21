e6272de512f773a70768390babe2879a6eb0a276
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 20:36:03 +0300

    Add ConditionWithMultipleValuesFactory

---
+++ Module.MusicBee.Extension.LibraryQuerying/Factories/Abstract/ConditionWithMultipleValuesFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Module.MusicBee.Extension.LibraryQuerying.Entities;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Factories.Abstract;
+
+internal delegate ConditionWithMultipleValues<TValue> ConditionWithMultipleValuesFactory<TValue>(
+    IField field,
+    Comparison comparison,
+    IReadOnlyCollection<TValue> values
+);
