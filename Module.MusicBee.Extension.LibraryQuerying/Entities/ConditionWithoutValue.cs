ae969097199cfb91476b8579a4ac2817f2dbb837
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 16:32:20 +0300

    Add ConditionWithoutValue

---
+++ Module.MusicBee.Extension.LibraryQuerying/Entities/ConditionWithoutValue.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Entities;
+
+internal sealed class ConditionWithoutValue : BaseCondition
+{
+    private readonly IField _field;
+    private readonly Comparison _comparison;
+
+    public ConditionWithoutValue(IField field, Comparison comparison)
+    {
+        _field = field;
+        _comparison = comparison;
+    }
+
+    public override XNode Build()
+    {
+        return new XElement(
+            "Condition",
+            new XAttribute("Field", _field.Name),
+            new XAttribute("Comparison", _comparison.Name)
+        );
+    }
+}
