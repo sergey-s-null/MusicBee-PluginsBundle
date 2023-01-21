4397dc6fc08345229100c714db97ccc755d9f7b4
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 22:04:46 +0300

    Add CombinedCondition, CombineMethod

---
+++ Module.MusicBee.Extension.LibraryQuerying/Entities/CombinedCondition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+using Module.MusicBee.Extension.LibraryQuerying.Enums;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Entities;
+
+internal sealed class CombinedCondition : BaseCondition
+{
+    public CombineMethod CombineMethod { get; }
+
+    private readonly IReadOnlyCollection<BaseCondition> _innerConditions;
+
+    public CombinedCondition(CombineMethod combineMethod, IReadOnlyCollection<BaseCondition> innerConditions)
+    {
+        CombineMethod = combineMethod;
+        _innerConditions = innerConditions;
+    }
+
+    public override XNode Build()
+    {
+        var andXElement = new XElement(
+            "And",
+            new XAttribute("CombineMethod", CombineMethod.XName)
+        );
+
+        foreach (var innerCondition in _innerConditions)
+        {
+            andXElement.Add(innerCondition.Build());
+        }
+
+        return new XElement(
+            "Condition",
+            new XAttribute("Field", Field.None.Name),
+            new XAttribute("Comparison", Comparison.StartsWith.Name),
+            new XAttribute("Value", string.Empty),
+            andXElement
+        );
+    }
+}
---
+++ Module.MusicBee.Extension.LibraryQuerying/Enums/CombineMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace Module.MusicBee.Extension.LibraryQuerying.Enums;
+
+internal sealed class CombineMethod
+{
+    public static readonly CombineMethod All = new("All");
+    public static readonly CombineMethod Any = new("Any");
+
+    public string XName { get; }
+
+    private CombineMethod(string xName)
+    {
+        XName = xName;
+    }
+}
