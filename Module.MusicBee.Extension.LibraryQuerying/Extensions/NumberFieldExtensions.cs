b71b05f518c324dd80a2fde31c020bc7c2c5b12e
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 16:48:47 +0300

    Add INumberField, NumberFieldExtensions

---
+++ Module.MusicBee.Extension.LibraryQuerying/Extensions/NumberFieldExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Extensions;
+
+public static class NumberFieldExtensions
+{
+    /// <summary>
+    /// Равно
+    /// </summary>
+    public static void Is(this INumberField field, int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не равно
+    /// </summary>
+    public static void IsNot(this INumberField field, int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Больше, чем
+    /// </summary>
+    public static void GreaterThan(this INumberField field, int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Меньше, чем
+    /// </summary>
+    public static void LessThan(this INumberField field, int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// В диапазоне
+    /// </summary>
+    public static void InRange(this INumberField field, int from, int to)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не в диапазоне
+    /// </summary>
+    public static void NotInRange(this INumberField field, int from, int to)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Не пустое значение
+    /// </summary>
+    public static void IsNotNull(this INumberField field)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Пустое значение
+    /// </summary>
+    public static void IsNull(this INumberField field)
+    {
+        throw new NotImplementedException();
+    }
+}
