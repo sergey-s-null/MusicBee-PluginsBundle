7ab0876a63516334346906abb710a299b083ab2a
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 20:10:27 +0300

    Add ConditionWithSingleValueFactory

---
+++ Module.MusicBee.Extension.LibraryQuerying/Factories/Abstract/ConditionWithSingleValueFactory.cs
@@ -0,0 +1,10 @@
+﻿using Module.MusicBee.Extension.LibraryQuerying.Entities;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Factories.Abstract;
+
+internal delegate ConditionWithSingleValue<TValue> ConditionWithSingleValueFactory<TValue>(
+    IField field,
+    Comparison comparison,
+    TValue value
+);
