0755c7f7b8c05bb50e60bdfcd627c2446d0dc78b
Sergey Semenov <Laiser399@mail.ru>
2023-01-21 20:16:36 +0300

    Add ConditionWithTwoValuesFactory

---
+++ Module.MusicBee.Extension.LibraryQuerying/Factories/Abstract/ConditionWithTwoValuesFactory.cs
@@ -0,0 +1,11 @@
+﻿using Module.MusicBee.Extension.LibraryQuerying.Entities;
+using Module.MusicBee.Extension.LibraryQuerying.Entities.Abstract;
+
+namespace Module.MusicBee.Extension.LibraryQuerying.Factories.Abstract;
+
+internal delegate ConditionWithTwoValues<TValue> ConditionWithTwoValuesFactory<TValue>(
+    IField field,
+    Comparison comparison,
+    TValue firstValue,
+    TValue secondValue
+);
