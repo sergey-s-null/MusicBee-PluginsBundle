c10dcfb555bc1de45be31028870a22be2107ff1b
Sergey Semenov <Laiser399@mail.ru>
2023-01-22 23:54:53 +0300

    Move some code to extensions

--- Module.MusicBee.MetaInfo/Extensions/MethodDefinitionExtensions.cs
+++ Module.MusicBee.MetaInfo/Extensions/MethodDefinitionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Module.MusicBee.MetaInfo.Entities;
+﻿using System.Linq;
+using Module.MusicBee.MetaInfo.Entities;
 
 namespace Module.MusicBee.MetaInfo.Extensions;
 
@@ -18,5 +19,23 @@ public static class MethodDefinitionExtensions
     public static bool HasReturnType(this MethodDefinition methodDefinition)
     {
         return methodDefinition.ReturnParameter.Type != typeof(void);
+    }
+
+    public static string GetClassMethodArguments(this MethodDefinition method)
+    {
+        var inParams = method.InputParameters
+            .Select(x => $"{x.GetCSharpTypeName()} {x.Name}");
+        var outParams = method.OutputParameters
+            .Select(x => $"out {x.GetCSharpTypeName()} {x.Name}");
+        return string.Join(", ", inParams.Concat(outParams));
+    }
+
+    public static string GetMethodCallingArguments(this MethodDefinition method)
+    {
+        var inParams = method.InputParameters
+            .Select(x => x.Name);
+        var outParams = method.OutputParameters
+            .Select(x => $"out {x.Name}");
+        return string.Join(", ", inParams.Concat(outParams));
     }
 }
