daca1a521b7879669ae909cd1a86ecd53474e274
Sergey Semenov <Laiser399@mail.ru>
2023-01-22 12:15:49 +0300

    Move IsEnumerable extension

--- CodeGenerator/Helpers/TypeHelper.cs
+++ CodeGenerator/Helpers/TypeHelper.cs
@@ -33,25 +33,6 @@ namespace CodeGenerator.Helpers
                 : type.Name;
         }
 
-        public static bool IsEnumerable(this Type type, out Type? elementType)
-        {
-            if (type.IsGenericType
-                && typeof(IEnumerable).IsAssignableFrom(type.GetGenericTypeDefinition()))
-            {
-                elementType = type.GenericTypeArguments.First();
-                return true;
-            }
-
-            if (type.IsArray && type.HasElementType)
-            {
-                elementType = type.GetElementType();
-                return true;
-            }
-
-            elementType = null;
-            return false;
-        }
-
         public static bool IsEnumerable(this Type type)
         {
             if (type.IsGenericType
---
+++ Module.MusicBee.MetaInfo.Proto/Extensions/TypeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Module.MusicBee.MetaInfo.Proto.Extensions;
+
+public static class TypeExtensions
+{
+    public static bool IsEnumerable(this Type type, out Type? elementType)
+    {
+        if (type.IsGenericType
+            && typeof(IEnumerable).IsAssignableFrom(type.GetGenericTypeDefinition()))
+        {
+            elementType = type.GenericTypeArguments.First();
+            return true;
+        }
+
+        if (type.IsArray && type.HasElementType)
+        {
+            elementType = type.GetElementType();
+            return true;
+        }
+
+        elementType = null;
+        return false;
+    }
+}
