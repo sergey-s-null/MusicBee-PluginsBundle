421875fd72e41110be90793a78980508f8b2f336
Sergey Semenov <Laiser399@mail.ru>
2023-01-22 13:39:48 +0300

    Move IsEnumerable method

--- Module.MusicBee.MetaInfo.Proto/Extensions/TypeExtensions.cs
+++ Module.MusicBee.MetaInfo.Proto/Extensions/TypeExtensions.cs
@@ -6,6 +6,22 @@ namespace Module.MusicBee.MetaInfo.Proto.Extensions;
 
 public static class TypeExtensions
 {
+    public static bool IsEnumerable(this Type type)
+    {
+        if (type.IsGenericType
+            && typeof(IEnumerable).IsAssignableFrom(type.GetGenericTypeDefinition()))
+        {
+            return true;
+        }
+
+        if (type.IsArray && type.HasElementType)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
     public static bool IsEnumerable(this Type type, out Type? elementType)
     {
         if (type.IsGenericType
