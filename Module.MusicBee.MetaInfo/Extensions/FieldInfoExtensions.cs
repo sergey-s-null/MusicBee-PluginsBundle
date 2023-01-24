bcb0c09958b4c6d79b83d18ff3b21f921e88c73e
Sergey Semenov <Laiser399@mail.ru>
2023-01-24 23:43:31 +0300

    Refactor MetaInfo

---
+++ Module.MusicBee.MetaInfo/Extensions/FieldInfoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+namespace Module.MusicBee.MetaInfo.Extensions;
+
+public static class FieldInfoExtensions
+{
+    public static bool IsDelegate(this FieldInfo fieldInfo)
+    {
+        return typeof(Delegate).IsAssignableFrom(fieldInfo.FieldType);
+    }
+}
