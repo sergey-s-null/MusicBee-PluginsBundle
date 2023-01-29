d5683e77d74c8b02d155134d6b2e1754761eb499
Sergey Semenov <Laiser399@mail.ru>
2023-01-29 17:23:03 +0300

    Fix copy scripts

--- copy_dll_debug.ps1
+++ copy_dll_debug.ps1
@@ -1,2 +1,2 @@
-Copy-Item ".\Plugin.Main\bin\Debug\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
-Copy-Item ".\Plugin.RemoteApi\bin\Debug\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
+Copy-Item ".\Plugin.Main\bin\Debug\net472\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
+Copy-Item ".\Plugin.RemoteApi\bin\Debug\net472\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
