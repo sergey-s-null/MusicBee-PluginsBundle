b8d18188283408dbac7154cad7fb0e85d3aa56b2
Sergey Semenov <Laiser399@mail.ru>
2023-01-29 11:12:02 +0300

    Remove Module.MusicBee.MetaInfo project

--- Root/Helpers/EnumerableHelper.cs
+++ Root/Helpers/EnumerableHelper.cs
@@ -21,12 +21,5 @@ namespace Root.Helpers
                 .Where(x => x is not null)
                 .Select(x => x!);
         }
-
-        public static IEnumerable<TDest> CastOrSkip<TSource, TDest>(this IEnumerable<TSource> items)
-        {
-            return items
-                .Where(x => x is TDest)
-                .Cast<TDest>();
-        }
     }
 }
