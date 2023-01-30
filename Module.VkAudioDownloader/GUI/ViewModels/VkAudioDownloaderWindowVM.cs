7cae0a8bddb31765edf9da09ed039e2a0a97b89c
Sergey Semenov <Laiser399@mail.ru>
2023-01-30 23:19:20 +0300

    Fix Module.VkAudioDownloader

--- Module.VkAudioDownloader/GUI/ViewModels/VkAudioDownloaderWindowVM.cs
+++ Module.VkAudioDownloader/GUI/ViewModels/VkAudioDownloaderWindowVM.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using Mead.MusicBee.Api.Services.Abstract;
 using Mead.MusicBee.Enums;
-using Module.MusicBee.Autogen.Services.Abstract;
 using Module.MusicBee.Extension.Helpers;
 using Module.Mvvm.Extension;
 using Module.Vk.Helpers;
