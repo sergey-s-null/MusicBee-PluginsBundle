48c23cecc89a137509df138f7c233f46314602c8
Sergey Semenov <Laiser399@mail.ru>
2023-02-05 01:01:53 +0300

    Remove Module.MusicBee.Extension.LibraryApi

--- Module.MusicBee.Extension.LibraryApi/Factories/Abstract/ReadOnlyMusicFileFactory.cs
+++
@@ -1,5 +0,0 @@
-﻿using Module.MusicBee.Extension.LibraryApi.Entities.Abstract;
-
-namespace Module.MusicBee.Extension.LibraryApi.Factories.Abstract;
-
-public delegate IReadOnlyMusicFile ReadOnlyMusicFileFactory(string path);
