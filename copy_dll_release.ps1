077b4743f7fa967989175c6cce84a38b1772941d
Sergey Semenov <Laiser399@mail.ru>
2023-02-13 23:40:20 +0300

    Remove RemoteApi plugin dll copying

--- copy_dll_release.ps1
+++ copy_dll_release.ps1
@@ -1,2 +1,1 @@
 Copy-Item ".\Plugin.Main\bin\Release\net472\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
-Copy-Item ".\Plugin.RemoteApi\bin\Release\net472\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
