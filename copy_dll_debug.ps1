af9c27a395cb215621f44d2250fc2df34fddee38
Sergey Semenov <sergey.s.null@gmail.com>
2023-02-18 21:05:48 +0300

    Remove remoting plugin (#8)
    * Remove Plugin.RemoteApi
    
    * Remove Module.RemoteMusicBeeApi
    
    * Remove MusicBeeApiClientWrapper
    
    * Remove Module.MusicBee.MetaInfo.Proto
    
    * Fix sln
    
    * Remove RemoteApi plugin dll copying

--- copy_dll_debug.ps1
+++ copy_dll_debug.ps1
@@ -1,2 +1,1 @@
 Copy-Item ".\Plugin.Main\bin\Debug\net472\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
-Copy-Item ".\Plugin.RemoteApi\bin\Debug\net472\*.dll" "C:\Program Files (x86)\MusicBee\Plugins"
