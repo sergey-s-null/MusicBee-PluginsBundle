22c8fded66d4b9658c5a8a475c5169c33827585c
Sergey Semenov <sergey.s.null@gmail.com>
2023-03-05 18:17:40 +0300

    Move AsyncEnumerableWrapper into separate file; refactor AudiosAsyncEnumerator.

---
+++ Module.VkAudioDownloader/Services/AsyncEnumerableWrapper.cs
@@ -0,0 +1,99 @@
+﻿using VkNet.Abstractions;
+using VkNet.Model.Attachments;
+using VkNet.Model.RequestParams;
+
+namespace Module.VkAudioDownloader.Services;
+
+public sealed class AsyncEnumerableWrapper : IAsyncEnumerable<Audio>
+{
+    private readonly IAudioCategory _audioCategory;
+
+    public AsyncEnumerableWrapper(IAudioCategory audioCategory)
+    {
+        _audioCategory = audioCategory;
+    }
+
+    public IAsyncEnumerator<Audio> GetAsyncEnumerator(CancellationToken cancellationToken = new CancellationToken())
+    {
+        return new AudiosAsyncEnumerator(async (offset, count) =>
+            await _audioCategory
+                .GetAsync(new AudioGetParams
+                {
+                    Offset = offset,
+                    Count = count
+                }));
+    }
+
+    private delegate Task<IReadOnlyCollection<Audio>> GetAudiosAsyncDelegate(
+        int offset,
+        int count
+    );
+
+    private sealed class AudiosAsyncEnumerator : IAsyncEnumerator<Audio>
+    {
+        private const int AudiosPerRequest = 10;
+
+        private Audio? _current;
+
+        public Audio Current => _current
+                                ?? throw new InvalidOperationException($"{nameof(Current)} is null");
+
+        private readonly GetAudiosAsyncDelegate _getAudiosAsync;
+        private int _offset;
+        private readonly Queue<Audio> _readyAudios = new();
+
+        public AudiosAsyncEnumerator(GetAudiosAsyncDelegate getAudiosAsync)
+        {
+            _getAudiosAsync = getAudiosAsync;
+        }
+
+        public async ValueTask<bool> MoveNextAsync()
+        {
+            if (_readyAudios.Count == 0)
+            {
+                if (!await TryLoadNextAudios())
+                {
+                    return false;
+                }
+            }
+
+            if (_readyAudios.Count == 0)
+            {
+                return false;
+            }
+
+            _current = _readyAudios.Dequeue();
+            return true;
+        }
+
+        private async Task<bool> TryLoadNextAudios()
+        {
+            try
+            {
+                var audios = await _getAudiosAsync(_offset, AudiosPerRequest);
+
+                _offset += audios.Count;
+                Enqueue(audios);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Enqueue(IEnumerable<Audio> audios)
+        {
+            foreach (var audio in audios)
+            {
+                _readyAudios.Enqueue(audio);
+            }
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return new ValueTask();
+        }
+    }
+}
