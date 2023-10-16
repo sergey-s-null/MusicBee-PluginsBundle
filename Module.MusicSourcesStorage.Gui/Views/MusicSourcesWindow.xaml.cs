08ec1be9754a4cf308989b2878e7df7f31cccbfc
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-16 21:36:42 +0300

    Add draft MusicSourcesWindow

---
+++ Module.MusicSourcesStorage.Gui/Views/MusicSourcesWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Module.MusicSourcesStorage.Gui.Views;
+
+public partial class MusicSourcesWindow : Window
+{
+    public MusicSourcesWindow()
+    {
+        InitializeComponent();
+    }
+}
