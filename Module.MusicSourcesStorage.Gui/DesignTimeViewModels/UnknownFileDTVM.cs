4fdab322513521dcec666ba9874490602346549d
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-17 18:45:31 +0300

    Add UnknownFile component

---
+++ Module.MusicSourcesStorage.Gui/DesignTimeViewModels/UnknownFileDTVM.cs
@@ -0,0 +1,21 @@
+﻿using Module.MusicSourcesStorage.Gui.AbstractViewModels;
+
+namespace Module.MusicSourcesStorage.Gui.DesignTimeViewModels;
+
+public sealed class UnknownFileDTVM : IUnknownFileVM
+{
+    public string Name { get; }
+    public string Path { get; }
+    public IList<INodeVM> ChildNodes { get; } = Array.Empty<INodeVM>();
+
+    // ReSharper disable once UnusedMember.Global
+    public UnknownFileDTVM() : this("some/path/to/unknown_file")
+    {
+    }
+
+    public UnknownFileDTVM(string path)
+    {
+        Name = System.IO.Path.GetFileName(path);
+        Path = path;
+    }
+}
