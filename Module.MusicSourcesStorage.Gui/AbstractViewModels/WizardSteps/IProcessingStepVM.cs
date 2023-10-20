8481b1b0a0b4b1cabbd47b412c2d461838e6911a
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-20 22:35:17 +0300

    Add ProcessingStep

---
+++ Module.MusicSourcesStorage.Gui/AbstractViewModels/WizardSteps/IProcessingStepVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+
+namespace Module.MusicSourcesStorage.Gui.AbstractViewModels.WizardSteps;
+
+public interface IProcessingStepVM : IAutomaticWizardStepVM
+{
+    string Text { get; }
+
+    ICommand Cancel { get; }
+}
