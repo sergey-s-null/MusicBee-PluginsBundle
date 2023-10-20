de9706af45d2055d9cc2b46febc1870646817f61
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-20 21:54:42 +0300

    Add custom next button text support

--- Module.MusicSourcesStorage.Gui/DesignTimeViewModels/WizardSteps/ManualWizardStepDTVM.cs
+++ Module.MusicSourcesStorage.Gui/DesignTimeViewModels/WizardSteps/ManualWizardStepDTVM.cs
@@ -10,11 +10,11 @@ public sealed class ManualWizardStepDTVM : IManualWizardStepVM
     public event EventHandler<StepTransitionEventArgs>? PreviousStepRequested;
     public event EventHandler? CloseWizardRequested;
 
-    public bool HasNextStep => false;
-    public bool CanGoNext => false;
-    public string? CustomNextStepName => null;
+    public bool HasNextStep => true;
+    public bool CanGoNext => true;
+    public string? CustomNextStepName => "Add";
 
-    public bool HasPreviousStep => true;
+    public bool HasPreviousStep => false;
     public bool CanGoBack => false;
 
     public ICommand Back => null!;
