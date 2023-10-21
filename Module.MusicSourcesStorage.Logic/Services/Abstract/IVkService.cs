a75a7b4942b7c6f0f0c9ef57afefaf015da76a65
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-21 20:54:35 +0300

    doc string

--- Module.MusicSourcesStorage.Logic/Services/Abstract/IVkService.cs
+++ Module.MusicSourcesStorage.Logic/Services/Abstract/IVkService.cs
@@ -1,9 +1,13 @@
 ﻿using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Exceptions;
 
 namespace Module.MusicSourcesStorage.Logic.Services.Abstract;
 
 public interface IVkService
 {
+    /// <exception cref="VkServiceException">
+    /// Error on get documents
+    /// </exception>
     Task<IReadOnlyList<VkDocument>> GetAttachedDocumentsFromPostAsync(
         ulong postOwnerId,
         ulong postId,
