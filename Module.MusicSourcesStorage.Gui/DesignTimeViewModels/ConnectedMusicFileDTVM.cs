cc6d0c94e67d6d0beb7d1e4bd77a413e18d6318c
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-21 15:12:15 +0300

    Add MusicFileDTVM

--- Module.MusicSourcesStorage.Gui/DesignTimeViewModels/ConnectedMusicFileDTVM.cs
+++ Module.MusicSourcesStorage.Gui/DesignTimeViewModels/ConnectedMusicFileDTVM.cs
@@ -3,22 +3,17 @@ using Module.MusicSourcesStorage.Gui.Enums;
 
 namespace Module.MusicSourcesStorage.Gui.DesignTimeViewModels;
 
-public sealed class ConnectedMusicFileDTVM : IConnectedMusicFileVM
+public sealed class ConnectedMusicFileDTVM : MusicFileDTVM, IConnectedMusicFileVM
 {
-    public string Name { get; }
-    public string Path { get; }
     public MusicFileState State { get; }
-    public IReadOnlyList<INodeVM> ChildNodes { get; } = Array.Empty<INodeVM>();
 
     // ReSharper disable once UnusedMember.Global
     public ConnectedMusicFileDTVM() : this("some/path/to/music.mp3", MusicFileState.InLibrary)
     {
     }
 
-    public ConnectedMusicFileDTVM(string path, MusicFileState state)
+    public ConnectedMusicFileDTVM(string path, MusicFileState state) : base(path)
     {
-        Name = System.IO.Path.GetFileName(path);
-        Path = path;
         State = state;
     }
 }
