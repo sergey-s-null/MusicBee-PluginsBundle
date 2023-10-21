1fc6c998a611915e7c594137433b2e207661b7ee
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-21 15:05:11 +0300

    Add two mappings support in HierarchyNodesViewSelector

--- Module.MusicSourcesStorage.Gui/Views/Components/HierarchyNodesViewSelector.cs
+++ Module.MusicSourcesStorage.Gui/Views/Components/HierarchyNodesViewSelector.cs
@@ -1,4 +1,6 @@
-﻿namespace Module.MusicSourcesStorage.Gui.Views.Components;
+﻿using System.Windows;
+
+namespace Module.MusicSourcesStorage.Gui.Views.Components;
 
 // Костыль - внутри HierarchicalDataTemplate невозможно выставить свойтво из статического контекст.
 // Не работает
@@ -7,8 +9,35 @@
 //    Binging ..., Source={StaticResource ...}
 public sealed class HierarchyNodesViewSelector : ViewSelector
 {
+    public static readonly DependencyProperty UseConnectedViewMappingProperty = DependencyProperty.Register(
+        nameof(UseConnectedViewMapping),
+        typeof(bool),
+        typeof(HierarchyNodesViewSelector),
+        new PropertyMetadata(
+            false,
+            (obj, _) =>
+            {
+                var selector = (HierarchyNodesViewSelector)obj;
+                selector.UpdateSelectionTable();
+            }
+        )
+    );
+
+    public bool UseConnectedViewMapping
+    {
+        get => (bool)GetValue(UseConnectedViewMappingProperty);
+        set => SetValue(UseConnectedViewMappingProperty, value);
+    }
+
     public HierarchyNodesViewSelector()
     {
-        SelectionTable = HierarchyNodesViewMapping.Map;
+        UpdateSelectionTable();
+    }
+
+    private void UpdateSelectionTable()
+    {
+        SelectionTable = UseConnectedViewMapping
+            ? HierarchyNodesViewMapping.ConnectedMap
+            : HierarchyNodesViewMapping.Map;
     }
 }
