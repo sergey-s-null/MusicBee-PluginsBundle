2f4c6c2c4032f5f526388201400e6e0a41e20b20
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-21 10:07:24 +0300

    Add IsReadOnly to Directory component

--- Module.MusicSourcesStorage.Gui/Views/Components/Directory.xaml.cs
+++ Module.MusicSourcesStorage.Gui/Views/Components/Directory.xaml.cs
@@ -1,11 +1,51 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Module.MusicSourcesStorage.Gui.Views.Components;
 
 public partial class Directory : UserControl
 {
+    public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register(
+        nameof(IsReadOnly),
+        typeof(bool),
+        typeof(Directory),
+        new PropertyMetadata(
+            false,
+            (obj, args) => OnIsReadOnlyChanged(
+                (Directory)obj,
+                (bool)args.OldValue,
+                (bool)args.NewValue
+            )
+        )
+    );
+
+    public bool IsReadOnly
+    {
+        get => (bool)GetValue(IsReadOnlyProperty);
+        set => SetValue(IsReadOnlyProperty, value);
+    }
+
     public Directory()
     {
         InitializeComponent();
+
+        UpdateContextMenuDisplayingState();
+    }
+
+    private void UpdateContextMenuDisplayingState()
+    {
+        ContextMenu = IsReadOnly
+            ? null
+            : new DirectoryContextMenu();
+    }
+
+    private static void OnIsReadOnlyChanged(Directory directory, bool oldValue, bool newValue)
+    {
+        if (oldValue == newValue)
+        {
+            return;
+        }
+
+        directory.UpdateContextMenuDisplayingState();
     }
 }
