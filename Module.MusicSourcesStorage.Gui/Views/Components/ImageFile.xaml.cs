eddd7390811595755be69eaddead7619d43e38dd
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-21 13:40:15 +0300

    Inherit ImageFile from HierarchyNodeBase

--- Module.MusicSourcesStorage.Gui/Views/Components/ImageFile.xaml.cs
+++ Module.MusicSourcesStorage.Gui/Views/Components/ImageFile.xaml.cs
@@ -1,11 +1,28 @@
-﻿using System.Windows.Controls;
+﻿namespace Module.MusicSourcesStorage.Gui.Views.Components;
 
-namespace Module.MusicSourcesStorage.Gui.Views.Components;
-
-public partial class ImageFile : UserControl
+public partial class ImageFile : HierarchyNodeBase
 {
     public ImageFile()
     {
         InitializeComponent();
+
+        UpdateContextMenuDisplayingState();
+    }
+
+    protected override void OnIsReadOnlyChanged(bool oldValue, bool newValue)
+    {
+        if (oldValue == newValue)
+        {
+            return;
+        }
+
+        UpdateContextMenuDisplayingState();
+    }
+
+    private void UpdateContextMenuDisplayingState()
+    {
+        ContextMenu = IsReadOnly
+            ? null
+            : new ImageFileContextMenu();
     }
 }
