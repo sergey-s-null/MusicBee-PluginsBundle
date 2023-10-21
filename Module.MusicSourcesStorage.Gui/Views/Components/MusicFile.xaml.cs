8cfac881f5f0514c2839dfe27adf63dafa31cb35
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-21 14:05:37 +0300

    Rework MusicFile to two states

--- Module.MusicSourcesStorage.Gui/Views/Components/MusicFile.xaml.cs
+++ Module.MusicSourcesStorage.Gui/Views/Components/MusicFile.xaml.cs
@@ -1,11 +1,55 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace Module.MusicSourcesStorage.Gui.Views.Components;
 
-public partial class MusicFile : UserControl
+public partial class MusicFile : HierarchyNodeBase
 {
     public MusicFile()
     {
         InitializeComponent();
+
+        UpdateComponentsDisplayingState();
+    }
+
+    protected override void OnIsReadOnlyChanged(bool oldValue, bool newValue)
+    {
+        if (oldValue == newValue)
+        {
+            return;
+        }
+
+        UpdateComponentsDisplayingState();
+    }
+
+    private void UpdateComponentsDisplayingState()
+    {
+        if (IsReadOnly)
+        {
+            ConfigureReadOnlyState();
+        }
+        else
+        {
+            ConfigureEditableState();
+        }
+    }
+
+    private void ConfigureReadOnlyState()
+    {
+        ContextMenu = null;
+        StateIconControl.Content = null;
+    }
+
+    private void ConfigureEditableState()
+    {
+        ContextMenu = new MusicFileContextMenu();
+        StateIconControl.Content = CreateStateIcon();
+    }
+
+    private static MusicFileStateIcon CreateStateIcon()
+    {
+        return new MusicFileStateIcon
+        {
+            Margin = new Thickness(6, 0, 4, 0)
+        };
     }
 }
---
+++ Module.MusicSourcesStorage.Gui/Views/Components/MusicFileStateIcon.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Module.MusicSourcesStorage.Gui.Views.Components;
+
+public partial class MusicFileStateIcon : UserControl
+{
+    public MusicFileStateIcon()
+    {
+        InitializeComponent();
+    }
+}
