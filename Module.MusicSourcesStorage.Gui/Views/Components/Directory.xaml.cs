b3b00fc06398515ba90e30b0b842c1e7a5fb42ab
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-21 13:37:38 +0300

    Inherit Directory from HierarchyNodeBase

--- Module.MusicSourcesStorage.Gui/Views/Components/Directory.xaml.cs
+++ Module.MusicSourcesStorage.Gui/Views/Components/Directory.xaml.cs
@@ -1,30 +1,7 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿namespace Module.MusicSourcesStorage.Gui.Views.Components;
 
-namespace Module.MusicSourcesStorage.Gui.Views.Components;
-
-public partial class Directory : UserControl
+public partial class Directory : HierarchyNodeBase
 {
-    public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register(
-        nameof(IsReadOnly),
-        typeof(bool),
-        typeof(Directory),
-        new PropertyMetadata(
-            false,
-            (obj, args) => OnIsReadOnlyChanged(
-                (Directory)obj,
-                (bool)args.OldValue,
-                (bool)args.NewValue
-            )
-        )
-    );
-
-    public bool IsReadOnly
-    {
-        get => (bool)GetValue(IsReadOnlyProperty);
-        set => SetValue(IsReadOnlyProperty, value);
-    }
-
     public Directory()
     {
         InitializeComponent();
@@ -32,20 +9,21 @@ public partial class Directory : UserControl
         UpdateContextMenuDisplayingState();
     }
 
-    private void UpdateContextMenuDisplayingState()
-    {
-        ContextMenu = IsReadOnly
-            ? null
-            : new DirectoryContextMenu();
-    }
-
-    private static void OnIsReadOnlyChanged(Directory directory, bool oldValue, bool newValue)
+    protected override void OnIsReadOnlyChanged(bool oldValue, bool newValue)
     {
         if (oldValue == newValue)
         {
             return;
         }
 
-        directory.UpdateContextMenuDisplayingState();
+        UpdateContextMenuDisplayingState();
+        throw new NotImplementedException();
+    }
+
+    private void UpdateContextMenuDisplayingState()
+    {
+        ContextMenu = IsReadOnly
+            ? null
+            : new DirectoryContextMenu();
     }
 }
---
+++ Module.MusicSourcesStorage.Gui/Views/Components/HierarchyNodeBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Module.MusicSourcesStorage.Gui.Views.Components;
+
+public abstract class HierarchyNodeBase : ContentControl
+{
+    public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register(
+        nameof(IsReadOnly),
+        typeof(bool),
+        typeof(ImageFile),
+        new PropertyMetadata(
+            false,
+            (obj, args) =>
+            {
+                var node = ((HierarchyNodeBase)obj);
+                node.OnIsReadOnlyChanged((bool)args.OldValue, (bool)args.NewValue);
+            }
+        )
+    );
+
+    public bool IsReadOnly
+    {
+        get => (bool)GetValue(IsReadOnlyProperty);
+        set => SetValue(IsReadOnlyProperty, value);
+    }
+
+    protected abstract void OnIsReadOnlyChanged(bool oldValue, bool newValue);
+}
