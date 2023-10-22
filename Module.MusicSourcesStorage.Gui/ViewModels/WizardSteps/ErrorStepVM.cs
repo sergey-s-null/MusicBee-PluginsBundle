08c5578fb73d8ed3510e7ba081adc9820c2539c0
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-22 23:36:49 +0300

    Add ErrorStepVM

---
+++ Module.MusicSourcesStorage.Gui/ViewModels/WizardSteps/ErrorStepVM.cs
@@ -0,0 +1,27 @@
+﻿using Module.MusicSourcesStorage.Gui.AbstractViewModels.WizardSteps;
+using Module.MusicSourcesStorage.Gui.Entities.Abstract;
+using Module.MusicSourcesStorage.Gui.Enums;
+
+namespace Module.MusicSourcesStorage.Gui.ViewModels.WizardSteps;
+
+public sealed class ErrorStepVM : IErrorStepVM
+{
+    public bool IsValidState => true;
+
+    public string Error { get; }
+
+    private readonly IWizardErrorContext _context;
+
+    public ErrorStepVM(IWizardErrorContext context)
+    {
+        _context = context;
+
+        Error = context.Error ?? "This is error without error";
+    }
+
+    public StepResult Confirm()
+    {
+        _context.Error = null;
+        return StepResult.Success;
+    }
+}
