b14e51a76802f34c0bbf3bba326a77e45992e3e6
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-22 23:41:30 +0300

    Add AddingVkPostWithArchiveContext

---
+++ Module.MusicSourcesStorage.Gui/Entities/AddingVkPostWithArchiveContext.cs
@@ -0,0 +1,14 @@
+﻿using Module.MusicSourcesStorage.Gui.Entities.Abstract;
+using Module.MusicSourcesStorage.Logic.Entities;
+
+namespace Module.MusicSourcesStorage.Gui.Entities;
+
+public sealed class AddingVkPostWithArchiveContext : IAddingVkPostWithArchiveContext
+{
+    public string? Error { get; set; }
+
+    public VkPostGlobalId? PostId { get; set; }
+    public IReadOnlyList<VkDocument>? AttachedDocuments { get; set; }
+    public VkDocument? SelectedDocument { get; set; }
+    public IReadOnlyList<MusicSourceFile>? IndexedFiles { get; set; }
+}
