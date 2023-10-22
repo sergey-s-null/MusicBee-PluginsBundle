19230167b2db327cea383dfacc0ec2b6dcb6a62e
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-23 00:02:53 +0300

    Add MapperProfile

---
+++ Module.MusicSourcesStorage.Gui/MapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Module.MusicSourcesStorage.Gui.AbstractViewModels.WizardSteps;
+using Module.MusicSourcesStorage.Gui.ViewModels.WizardSteps;
+using Module.MusicSourcesStorage.Logic.Entities;
+
+namespace Module.MusicSourcesStorage.Gui;
+
+public sealed class MapperProfile : Profile
+{
+    public MapperProfile()
+    {
+        CreateMap<VkDocument, IVkDocumentVM>()
+            .ConstructUsing(x => new VkDocumentVM(x.Name));
+    }
+}
