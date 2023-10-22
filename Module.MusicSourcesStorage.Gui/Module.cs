8b06efcee125f4cb021cf4979e13b0417e0dd60a
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-23 00:03:03 +0300

    Add Module

---
+++ Module.MusicSourcesStorage.Gui/Module.cs
@@ -0,0 +1,61 @@
+﻿using Autofac;
+using Module.MusicSourcesStorage.Gui.AbstractViewModels;
+using Module.MusicSourcesStorage.Gui.AbstractViewModels.WizardSteps;
+using Module.MusicSourcesStorage.Gui.Enums;
+using Module.MusicSourcesStorage.Gui.ViewModels;
+using Module.MusicSourcesStorage.Gui.ViewModels.WizardSteps;
+using Module.MusicSourcesStorage.Gui.Views;
+
+namespace Module.MusicSourcesStorage.Gui;
+
+public class Module : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        RegisterViews(builder);
+        RegisterViewModels(builder);
+        RegisterStepViewModels(builder);
+    }
+
+    private static void RegisterViews(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<Wizard>()
+            .AsSelf();
+    }
+
+    private static void RegisterViewModels(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<WizardVM>()
+            .As<IWizardVM>();
+    }
+
+    private static void RegisterStepViewModels(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<SuccessResultStepVM>()
+            .Keyed<IWizardStepVM>(StepType.SuccessResult);
+        builder
+            .RegisterType<ErrorStepVM>()
+            .Keyed<IWizardStepVM>(StepType.Error);
+        builder
+            .RegisterType<SelectVkPostStepVM>()
+            .Keyed<IWizardStepVM>(StepType.SelectVkPost);
+        builder
+            .RegisterType<ReceiveVkPostDocumentsStepVM>()
+            .Keyed<IWizardStepVM>(StepType.ReceiveVkPostDocumentsStepVM);
+        builder
+            .RegisterType<SelectDocumentFromVkPostStepVM>()
+            .Keyed<IWizardStepVM>(StepType.SelectDocumentFromVkPost);
+        builder
+            .RegisterType<DownloadAndIndexArchiveStepVM>()
+            .Keyed<IWizardStepVM>(StepType.DownloadAndIndexArchive);
+        builder
+            .RegisterType<IndexingResultStepVM>()
+            .Keyed<IWizardStepVM>(StepType.IndexingResult);
+        builder
+            .RegisterType<AddMusicSourceToDatabaseStepVM>()
+            .Keyed<IWizardStepVM>(StepType.AddMusicSourceToDatabase);
+    }
+}
