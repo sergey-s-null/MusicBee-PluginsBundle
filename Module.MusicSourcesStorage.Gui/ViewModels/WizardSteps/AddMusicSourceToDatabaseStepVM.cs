e2748b78c39a50bb0c055966b2af8efda97b4dac
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-22 13:29:09 +0300

    Add AddMusicSourceToDatabaseStepVM

---
+++ Module.MusicSourcesStorage.Gui/ViewModels/WizardSteps/AddMusicSourceToDatabaseStepVM.cs
@@ -0,0 +1,22 @@
+﻿using Module.MusicSourcesStorage.Gui.AbstractViewModels.WizardSteps;
+using Module.MusicSourcesStorage.Gui.Factories.Abstract;
+
+namespace Module.MusicSourcesStorage.Gui.ViewModels.WizardSteps;
+
+public sealed class AddMusicSourceToDatabaseStepVM : ProcessingStepBaseVM
+{
+    public override bool CanSafelyCloseWizard { get; protected set; }
+
+    public override string Text { get; protected set; }
+
+    public AddMusicSourceToDatabaseStepVM(IWizardCommonStepsFactory commonStepsFactory) : base(commonStepsFactory)
+    {
+        CanSafelyCloseWizard = false;
+        Text = "Adding music source to database";
+    }
+
+    protected override Task<IWizardStepVM> ProcessAsync(CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+}
