b20d11341cfca7679d6639ac124ff63fa490b2f3
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-22 20:51:48 +0300

    -

--- Module.MusicSourcesStorage.Gui/ViewModels/WizardSteps/AddMusicSourceToDatabaseStepVM.cs
+++ Module.MusicSourcesStorage.Gui/ViewModels/WizardSteps/AddMusicSourceToDatabaseStepVM.cs
@@ -6,7 +6,7 @@ public sealed class AddMusicSourceToDatabaseStepVM : ProcessingStepBaseVM
 
     public AddMusicSourceToDatabaseStepVM()
     {
-        Text = "Adding music source to database";
+        Text = "Starting";
     }
 
     protected override Task ProcessAsync(CancellationToken token)
