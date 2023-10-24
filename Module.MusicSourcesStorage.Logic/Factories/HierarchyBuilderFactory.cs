2b5f36b4fea8acd227201e1541b84f753d6d06dc
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-24 23:00:46 +0300

    Add HierarchyBuilderFactory

---
+++ Module.MusicSourcesStorage.Logic/Factories/HierarchyBuilderFactory.cs
@@ -0,0 +1,8 @@
+﻿using Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Factories;
+
+public delegate IHierarchyBuilder<TValue, TPathElement> HierarchyBuilderFactory<TValue, TPathElement>(
+    Func<TValue, IReadOnlyList<TPathElement>> pathElementsFactory,
+    IEqualityComparer<TPathElement> pathElementEqualityComparer
+);
