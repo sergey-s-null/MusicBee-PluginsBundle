f9aae8da8669a5ef1af3352a9f4c7c1162fbbde4
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-24 23:03:04 +0300

    Add NodesHierarchyVMBuilder

---
+++ Module.MusicSourcesStorage.Gui/Factories/Abstract/IHierarchyNodeVMFactory.cs
@@ -0,0 +1,14 @@
+﻿using Module.MusicSourcesStorage.Gui.AbstractViewModels;
+
+namespace Module.MusicSourcesStorage.Gui.Factories.Abstract;
+
+public interface IHierarchyNodeVMFactory
+{
+    IDirectoryVM CreateDirectoryVM(string name, IReadOnlyList<INodeVM>? childNodes = null);
+
+    IMusicFileVM CreateMusicFileVM(string name, string path);
+
+    IImageFileVM CreateImageFileVM(string name, string path);
+
+    IUnknownFileVM CreateUnknownFileVM(string name, string path);
+}
---
+++ Module.MusicSourcesStorage.Gui/Services/NodesHierarchyVMBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using Module.MusicSourcesStorage.Gui.AbstractViewModels;
+using Module.MusicSourcesStorage.Gui.Factories.Abstract;
+using Module.MusicSourcesStorage.Gui.Services.Abstract;
+using Module.MusicSourcesStorage.Gui.ViewModels;
+using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Factories;
+using Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Gui.Services;
+
+public sealed class NodesHierarchyVMBuilder : INodesHierarchyVMBuilder
+{
+    private readonly IHierarchyNodeVMFactory _hierarchyNodeVMFactory;
+    private readonly IHierarchyBuilder<MusicSourceFile, string> _hierarchyBuilder;
+
+    public NodesHierarchyVMBuilder(
+        IHierarchyNodeVMFactory hierarchyNodeVMFactory,
+        HierarchyBuilderFactory<MusicSourceFile, string> hierarchyBuilderFactory)
+    {
+        _hierarchyNodeVMFactory = hierarchyNodeVMFactory;
+        _hierarchyBuilder = hierarchyBuilderFactory(
+            x => x.Path.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar),
+            StringComparer.InvariantCultureIgnoreCase
+        );
+    }
+
+    public INodesHierarchyVM Build(IReadOnlyList<MusicSourceFile> files)
+    {
+        _hierarchyBuilder.Build(files, out var rootNodes, out var rootLeaves);
+
+        return new NodesHierarchyVM(CreateNodeViewModels(rootNodes, rootLeaves));
+    }
+
+    private IReadOnlyList<INodeVM> CreateNodeViewModels(
+        IReadOnlyList<Node<MusicSourceFile, string>> nodes,
+        IReadOnlyList<Leaf<MusicSourceFile, string>> leaves)
+    {
+        return nodes
+            .Select(CreateNodeVM)
+            .Concat(leaves
+                .Select(x => CreateNodeVM(x.Value))
+            )
+            .ToList();
+    }
+
+    private INodeVM CreateNodeVM(Node<MusicSourceFile, string> node)
+    {
+        var childNodes = CreateNodeViewModels(node.ChildNodes, node.Leaves);
+
+        return _hierarchyNodeVMFactory.CreateDirectoryVM(node.PathElement, childNodes);
+    }
+
+    private INodeVM CreateNodeVM(MusicSourceFile file)
+    {
+        // todo classify
+        return _hierarchyNodeVMFactory.CreateUnknownFileVM("<blank>", file.Path);
+    }
+}
