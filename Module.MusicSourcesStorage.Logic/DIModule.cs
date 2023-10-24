3ec4c1bdff4a997c00426fba07b1e1e6ea150105
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-24 23:17:56 +0300

    Add DI module for Logic project

---
+++ Module.MusicSourcesStorage.Logic/DIModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Module.MusicSourcesStorage.Logic.Services;
+using Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic;
+
+public sealed class DIModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<VkService>()
+            .As<IVkService>()
+            .SingleInstance();
+        builder
+            .RegisterType<VkDocumentDownloader>()
+            .As<IVkDocumentDownloader>()
+            .SingleInstance();
+        builder
+            .RegisterType<ArchiveIndexer>()
+            .As<IArchiveIndexer>()
+            .SingleInstance();
+        builder
+            .RegisterGeneric(typeof(HierarchyBuilder<,>))
+            .As(typeof(IHierarchyBuilder<,>));
+    }
+}
