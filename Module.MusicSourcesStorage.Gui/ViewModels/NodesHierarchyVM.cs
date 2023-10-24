124c427452e43d57691edaca00037b9158158795
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-24 23:07:11 +0300

    Add NodesHierarchyVM

---
+++ Module.MusicSourcesStorage.Gui/ViewModels/NodesHierarchyVM.cs
@@ -0,0 +1,15 @@
+﻿using Module.MusicSourcesStorage.Gui.AbstractViewModels;
+using PropertyChanged;
+
+namespace Module.MusicSourcesStorage.Gui.ViewModels;
+
+[AddINotifyPropertyChangedInterface]
+public sealed class NodesHierarchyVM : INodesHierarchyVM
+{
+    public IReadOnlyList<INodeVM> RootNodes { get; }
+
+    public NodesHierarchyVM(IReadOnlyList<INodeVM> rootNodes)
+    {
+        RootNodes = rootNodes;
+    }
+}
