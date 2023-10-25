7708646045e6953f01cecbeedfd14db3a3db664f
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-25 18:35:05 +0300

    Move some code to Test.Common project

---
+++ Test.Common/VkHelper.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VkNet;
+using VkNet.Abstractions;
+using VkNet.AudioBypassService.Extensions;
+using VkNet.Model;
+
+namespace Test.Common;
+
+public static class VkHelper
+{
+    private static readonly string TokenFilePath =
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "vk_secret.txt");
+
+    public static IVkApi GetAuthorizedVkApi()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddAudioBypass();
+        var vkApi = new VkApi(serviceCollection);
+        AuthorizeWithStoredToken(vkApi);
+        return vkApi;
+    }
+
+    private static void AuthorizeWithStoredToken(IVkApi vkApi)
+    {
+        var authParams = GetVkAuthParamsWithStoredToken();
+        vkApi.Authorize(authParams);
+        SaveToken(vkApi.Token);
+    }
+
+    private static ApiAuthParams GetVkAuthParamsWithStoredToken()
+    {
+        // todo delete secrets
+        const string login = "";
+        const string password = "";
+
+        if (TryLoadToken(out var token))
+        {
+            Console.WriteLine("Token loaded.");
+            return MakeAuthParams(login, password, token);
+        }
+
+        Console.WriteLine("Could not load token. Auth with default credentials.");
+        return MakeAuthParams(login, password);
+    }
+
+    private static bool TryLoadToken(out string token)
+    {
+        try
+        {
+            LoadToken(out token);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Could not load token: {e}");
+            token = "";
+            return false;
+        }
+    }
+
+    private static void LoadToken(out string token)
+    {
+        token = File.ReadAllText(TokenFilePath);
+    }
+
+    private static void SaveToken(string token)
+    {
+        File.WriteAllText(TokenFilePath, token);
+    }
+
+    private static ApiAuthParams MakeAuthParams(string login, string password, string? token = null)
+    {
+        var authParams = new ApiAuthParams
+        {
+            Login = login,
+            Password = password,
+            TwoFactorAuthorization = () =>
+            {
+                Console.Write("Code? ");
+                return Console.ReadLine();
+            }
+        };
+
+        if (token is not null)
+        {
+            authParams.AccessToken = token;
+        }
+
+        return authParams;
+    }
+}
--- Test.Console/Program.cs
+++ Test.Console/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System.Net;
 using System.Text.RegularExpressions;
-using Microsoft.Extensions.DependencyInjection;
-using VkNet;
-using VkNet.Abstractions;
-using VkNet.AudioBypassService.Extensions;
-using VkNet.Model;
+using Test.Common;
 using VkNet.Model.RequestParams;
 
 namespace Test.Console;
 
 internal static class Program
 {
-    private static readonly string TokenFilePath =
-        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "vk_secret.txt");
-
     private static readonly string M3U8FilePath = Path.GetFullPath(@"../../delete_this/index.m3u8");
     private static readonly string BaseUrlFilePath = Path.GetFullPath(@"../../delete_this/baseUrl.txt");
 
@@ -24,7 +17,7 @@ internal static class Program
     // download m3u8 and baseUrl
     private static void Part1()
     {
-        var api = GetAuthorizedVkApi();
+        var api = VkHelper.GetAuthorizedVkApi();
 
         // TODO change indices
         var audio = api.Audio.Get(new AudioGetParams() { Offset = 1, Count = 1 })[0];
@@ -39,91 +32,5 @@ internal static class Program
         var match = regex.Match(audio.Url.AbsoluteUri);
         var baseUrl = match.Groups[1].Value;
         File.WriteAllText(BaseUrlFilePath, baseUrl);
-    }
-
-    private static IVkApi GetAuthorizedVkApi()
-    {
-        var serviceCollection = new ServiceCollection();
-        serviceCollection.AddAudioBypass();
-        var vkApi = new VkApi(serviceCollection);
-        AuthorizeWithStoredToken(vkApi);
-        return vkApi;
-    }
-
-    private static void AuthorizeWithStoredToken(IVkApi vkApi)
-    {
-        var authParams = GetVkAuthParamsWithStoredToken();
-        vkApi.Authorize(authParams);
-        SaveToken(vkApi.Token);
-    }
-
-    private static ApiAuthParams GetVkAuthParamsWithStoredToken()
-    {
-        // todo delete secrets
-        const string login = "";
-        const string password = "";
-
-        if (TryLoadToken(out var token))
-        {
-            System.Console.WriteLine("Token loaded.");
-            return MakeAuthParams(login, password, token);
-        }
-
-        System.Console.WriteLine("Could not load token. Auth with default credentials.");
-        return MakeAuthParams(login, password);
-    }
-
-    private static bool TryLoadToken(out string token)
-    {
-        try
-        {
-            LoadToken(out token);
-            return true;
-        }
-        catch (Exception e)
-        {
-            System.Console.WriteLine($"Could not load token: {e}");
-            token = "";
-            return false;
-        }
-    }
-
-    private static void LoadToken(out string token)
-    {
-        token = File.ReadAllText(TokenFilePath);
-    }
-
-    private static void SaveToken(string token)
-    {
-        File.WriteAllText(TokenFilePath, token);
-    }
-
-    private static ApiAuthParams MakeAuthParams(string login, string password)
-    {
-        return new ApiAuthParams
-        {
-            Login = login,
-            Password = password,
-            TwoFactorAuthorization = () =>
-            {
-                System.Console.Write("Code? ");
-                return System.Console.ReadLine();
-            }
-        };
-    }
-
-    private static ApiAuthParams MakeAuthParams(string login, string password, string token)
-    {
-        return new ApiAuthParams
-        {
-            Login = login,
-            Password = password,
-            TwoFactorAuthorization = () =>
-            {
-                System.Console.Write("Code? ");
-                return System.Console.ReadLine();
-            },
-            AccessToken = token
-        };
     }
 }
