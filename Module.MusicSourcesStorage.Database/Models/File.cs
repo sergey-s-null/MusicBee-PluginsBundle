ae60bc84847d87d5a781f02447f2616cd5a861ee
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-27 23:02:39 +0300

    Add Size property to File model

--- Module.MusicSourcesStorage.Database/Models/File.cs
+++ Module.MusicSourcesStorage.Database/Models/File.cs
@@ -6,6 +6,7 @@ public abstract class File
 {
     public int Id { get; set; }
     [Required] public string Path { get; set; } = string.Empty;
+    [Required] public long Size { get; set; }
 
     [Required] public MusicSource? Source { get; set; }
 }
