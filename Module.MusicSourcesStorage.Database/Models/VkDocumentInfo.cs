98d7bfed5ce88aabcd5542e4493d0fb6f38853ec
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-27 22:48:04 +0300

    Add comments

--- Module.MusicSourcesStorage.Database/Models/VkDocumentInfo.cs
+++ Module.MusicSourcesStorage.Database/Models/VkDocumentInfo.cs
@@ -5,8 +5,16 @@ namespace Module.MusicSourcesStorage.Database.Models;
 [ComplexType]
 public sealed class VkDocumentInfo
 {
+    /// <summary>
+    /// Document id in VK context.
+    /// </summary>
     public long Id { get; set; }
+
+    /// <summary>
+    /// Document owner id in VK context.
+    /// </summary>
     public long OwnerId { get; set; }
+
     public string Name { get; set; } = string.Empty;
     public string Uri { get; set; } = string.Empty;
     public long? Size { get; set; }
--- Module.MusicSourcesStorage.Database/Models/VkPostInfo.cs
+++ Module.MusicSourcesStorage.Database/Models/VkPostInfo.cs
@@ -5,6 +5,13 @@ namespace Module.MusicSourcesStorage.Database.Models;
 [ComplexType]
 public sealed class VkPostInfo
 {
+    /// <summary>
+    /// Id of post in VK context.
+    /// </summary>
     public ulong Id { get; set; }
+
+    /// <summary>
+    /// Post owner id in VK context.
+    /// </summary>
     public ulong OwnerId { get; set; }
 }
