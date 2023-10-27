ec051498076f576b60bc0912a838121cb26230de
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-27 23:02:25 +0300

    Cover -> ImageFile

--- Module.MusicSourcesStorage.Database/Models/Cover.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Module.MusicSourcesStorage.Database.Models;
-
-public sealed class Cover : File
-{
-    [Required] public byte[] Data { get; set; } = Array.Empty<byte>();
-}
---
+++ Module.MusicSourcesStorage.Database/Models/ImageFile.cs
@@ -0,0 +1,7 @@
+﻿namespace Module.MusicSourcesStorage.Database.Models;
+
+public sealed class ImageFile : File
+{
+    public bool IsCover { get; set; }
+    public byte[]? Data { get; set; } = Array.Empty<byte>();
+}
