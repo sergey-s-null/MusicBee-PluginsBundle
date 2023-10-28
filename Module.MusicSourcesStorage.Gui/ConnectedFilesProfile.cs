6a95c9db2e2ee8ba4b3a3d8fd6a8b620bcb1ce7c
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-28 16:02:56 +0300

    Add nodes mapping profiles

---
+++ Module.MusicSourcesStorage.Gui/ConnectedFilesProfile.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using AutoMapper;
+using Module.MusicSourcesStorage.Gui.AbstractViewModels.Nodes;
+using Module.MusicSourcesStorage.Gui.Factories;
+using Module.MusicSourcesStorage.Logic.Entities;
+
+namespace Module.MusicSourcesStorage.Gui;
+
+// todo move profiles to special folder
+public sealed class ConnectedFilesProfile : Profile
+{
+    public ConnectedFilesProfile(ILifetimeScope lifetimeScope)
+    {
+        var connectedMusicFileVMFactory = lifetimeScope.Resolve<ConnectedMusicFileVMFactory>();
+        var connectedImageFileVMFactory = lifetimeScope.Resolve<ConnectedImageFileVMFactory>();
+        var connectedUnknownFileVMFactory = lifetimeScope.Resolve<ConnectedUnknownFileVMFactory>();
+
+        CreateMap<SourceFile, IFileVM>()
+            .Include<MusicFile, IConnectedMusicFileVM>()
+            .Include<ImageFile, IConnectedImageFileVM>()
+            .Include<UnknownFile, IConnectedUnknownFileVM>();
+
+        CreateMap<MusicFile, IConnectedMusicFileVM>()
+            .ConstructUsing(x => connectedMusicFileVMFactory(x.Path));
+        CreateMap<ImageFile, IConnectedImageFileVM>()
+            .ConstructUsing(x => connectedImageFileVMFactory(x.Path));
+        CreateMap<UnknownFile, IConnectedUnknownFileVM>()
+            .ConstructUsing(x => connectedUnknownFileVMFactory(x.Path));
+    }
+}
---
+++ Module.MusicSourcesStorage.Gui/NotConnectedFilesProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Module.MusicSourcesStorage.Gui.AbstractViewModels.Nodes;
+using Module.MusicSourcesStorage.Gui.ViewModels.Nodes;
+using Module.MusicSourcesStorage.Logic.Entities;
+
+namespace Module.MusicSourcesStorage.Gui;
+
+public sealed class NotConnectedFilesProfile : Profile
+{
+    public NotConnectedFilesProfile()
+    {
+        CreateMap<SourceFile, IFileVM>()
+            .Include<MusicFile, IMusicFileVM>()
+            .Include<ImageFile, IImageFileVM>()
+            .Include<UnknownFile, IUnknownFileVM>();
+
+        CreateMap<MusicFile, IMusicFileVM>()
+            .ConstructUsing(x => new MusicFileVM(x.Path));
+        CreateMap<ImageFile, IImageFileVM>()
+            .ConstructUsing(x => new ImageFileVM(x.Path));
+        CreateMap<UnknownFile, IUnknownFileVM>()
+            .ConstructUsing(x => new UnknownFileVM(x.Path));
+    }
+}
