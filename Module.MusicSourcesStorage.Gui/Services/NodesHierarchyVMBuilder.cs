bb0c201a63a2fd46331ccbedb2839fd81ea2e3de
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-28 13:44:05 +0300

    Fix

--- Module.MusicSourcesStorage.Gui/Services/NodesHierarchyVMBuilder.cs
+++ Module.MusicSourcesStorage.Gui/Services/NodesHierarchyVMBuilder.cs
@@ -5,7 +5,7 @@ using Module.MusicSourcesStorage.Gui.Factories.Abstract;
 using Module.MusicSourcesStorage.Gui.Services.Abstract;
 using Module.MusicSourcesStorage.Gui.ViewModels;
 using Module.MusicSourcesStorage.Logic.Entities;
-using Module.MusicSourcesStorage.Logic.Factories;
+using Module.MusicSourcesStorage.Logic.Factories.Abstract;
 using Module.MusicSourcesStorage.Logic.Services.Abstract;
 
 namespace Module.MusicSourcesStorage.Gui.Services;
@@ -18,11 +18,11 @@ public sealed class NodesHierarchyVMBuilder : INodesHierarchyVMBuilder
 
     public NodesHierarchyVMBuilder(
         INodeVMFactory nodeVMFactory,
-        HierarchyBuilderFactory<IndexedFile, string> hierarchyBuilderFactory,
+        IHierarchyBuilderFactory hierarchyBuilderFactory,
         INodeVMBuilder nodeVMBuilder)
     {
         _nodeVMFactory = nodeVMFactory;
-        _hierarchyBuilder = hierarchyBuilderFactory(
+        _hierarchyBuilder = hierarchyBuilderFactory.Create<IndexedFile, string>(
             x => x.Path.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar),
             StringComparer.InvariantCultureIgnoreCase
         );
