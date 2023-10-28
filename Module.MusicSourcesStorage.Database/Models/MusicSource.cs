99173667bd86240e78fc21ff09186b053116ea83
Sergey Semenov <sergey.s.null@gmail.com>
2023-10-28 12:47:57 +0300

    Use List in db model

--- Module.MusicSourcesStorage.Database/Models/MusicSource.cs
+++ Module.MusicSourcesStorage.Database/Models/MusicSource.cs
@@ -7,5 +7,5 @@ public abstract class MusicSource
     public int Id { get; set; }
     [Required] public string Name { get; set; } = string.Empty;
 
-    public IList<File> Files { get; set; } = new List<File>();
+    public List<File> Files { get; set; } = new();
 }
