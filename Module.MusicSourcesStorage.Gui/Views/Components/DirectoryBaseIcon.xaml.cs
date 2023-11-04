6b9dca1b43ed0c9024ba07504c5c3c58b8978061
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-04 13:37:45 +0300

    Rework DirectoryIcon

---
+++ Module.MusicSourcesStorage.Gui/Views/Components/DirectoryBaseIcon.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Module.MusicSourcesStorage.Gui.Views.Components;
+
+public partial class DirectoryBaseIcon : UserControl
+{
+    public DirectoryBaseIcon()
+    {
+        InitializeComponent();
+    }
+}
