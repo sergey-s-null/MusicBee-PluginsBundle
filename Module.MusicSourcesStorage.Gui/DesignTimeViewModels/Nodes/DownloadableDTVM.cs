31733887b5df6fcfea0047700adc90b03762748e
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-06 14:24:52 +0300

    Add DownloadStateIcon

---
+++ Module.MusicSourcesStorage.Gui/DesignTimeViewModels/Nodes/DownloadableDTVM.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+using Module.MusicSourcesStorage.Gui.AbstractViewModels.Nodes;
+
+namespace Module.MusicSourcesStorage.Gui.DesignTimeViewModels.Nodes;
+
+public sealed class DownloadableDTVM : IDownloadableVM
+{
+    public bool CanDownload => false;
+    public bool IsDownloaded => true;
+
+    public ICommand Download => null!;
+}
---
+++ Module.MusicSourcesStorage.Gui/Views/Nodes/DownloadStateIcon.xaml.cs
@@ -0,0 +1,11 @@
+﻿using MahApps.Metro.IconPacks;
+
+namespace Module.MusicSourcesStorage.Gui.Views.Nodes;
+
+public partial class DownloadStateIcon : PackIconUnicons
+{
+    public DownloadStateIcon()
+    {
+        InitializeComponent();
+    }
+}
