eec606a2e3345a64247b92269a4064395a034696
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-10 19:17:51 +0300

    Add VkArchiveFilesDownloadingService

---
+++ Module.MusicSourcesStorage.Logic/Services/Abstract/IVkArchiveFilesDownloadingService.cs
@@ -0,0 +1,15 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+public interface IVkArchiveFilesDownloadingService
+{
+    /// <returns>Downloaded file path.</returns>
+    IMultiStepTaskWithProgress<string> DownloadAsync(
+        VkPostWithArchiveSource source,
+        SourceFile file,
+        bool activateTask,
+        CancellationToken token = default
+    );
+}
---
+++ Module.MusicSourcesStorage.Logic/Services/VkArchiveFilesDownloadingService.cs
@@ -0,0 +1,49 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+using Module.MusicSourcesStorage.Logic.Extensions;
+using Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Services;
+
+public sealed class VkArchiveFilesDownloadingService : IVkArchiveFilesDownloadingService
+{
+    private readonly ISourceFilesPathService _sourceFilesPathService;
+    private readonly IVkDocumentDownloadingTaskManager _vkDocumentDownloadingTaskManager;
+    private readonly IArchiveExtractor _archiveExtractor;
+
+    public VkArchiveFilesDownloadingService(
+        ISourceFilesPathService sourceFilesPathService,
+        IVkDocumentDownloadingTaskManager vkDocumentDownloadingTaskManager,
+        IArchiveExtractor archiveExtractor)
+    {
+        _sourceFilesPathService = sourceFilesPathService;
+        _vkDocumentDownloadingTaskManager = vkDocumentDownloadingTaskManager;
+        _archiveExtractor = archiveExtractor;
+    }
+
+    public IMultiStepTaskWithProgress<string> DownloadAsync(
+        VkPostWithArchiveSource source,
+        SourceFile file,
+        bool activateTask,
+        CancellationToken token = default)
+    {
+        var targetFilePath = _sourceFilesPathService.GetDownloadingFilePath(source, file);
+
+        var task = _vkDocumentDownloadingTaskManager.GetOrCreateNewAsync(source.Document, false, token)
+            .Chain(archiveFilePath => _archiveExtractor.ExtractAsync(
+                archiveFilePath,
+                file.Path,
+                targetFilePath,
+                true,
+                false,
+                token
+            ));
+
+        if (activateTask)
+        {
+            task.Activate();
+        }
+
+        return task;
+    }
+}
