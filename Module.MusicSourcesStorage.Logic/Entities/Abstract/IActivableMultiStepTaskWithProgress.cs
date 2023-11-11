b7dc997ad667b1792f11d03c46fd709bd6d03ca6
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 19:59:41 +0300

    Add IActivableMultiStepTaskWithProgress

---
+++ Module.MusicSourcesStorage.Logic/Entities/Abstract/IActivableMultiStepTaskWithProgress.cs
@@ -0,0 +1,11 @@
+﻿namespace Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+public interface IActivableMultiStepTaskWithProgress<TResult> : IMultiStepTaskWithProgress<TResult>
+{
+    void Activate(CancellationToken token = default);
+}
+
+public interface IActivableMultiStepTaskWithProgress<in TArgs, TResult> : IMultiStepTaskWithProgress<TResult>
+{
+    void Activate(TArgs args, CancellationToken token = default);
+}
