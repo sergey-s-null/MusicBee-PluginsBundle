b8ed6cf6609570291748ee68079d8144aab82ae0
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 14:49:20 +0300

    forgotten

---
+++ Module.MusicSourcesStorage.Logic/Extensions/ActivableWithoutCancellationTaskWithProgressExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Extensions;
+
+public static class ActivableWithoutCancellationTaskWithProgressExtensions
+{
+    public static IActivableWithoutCancellationTaskWithProgress<TResult> Activated<TResult>(
+        this IActivableWithoutCancellationTaskWithProgress<TResult> task)
+    {
+        if (task.IsActivated)
+        {
+            return task;
+        }
+
+        task.Activate();
+        return task;
+    }
+
+    public static IActivableWithoutCancellationTaskWithProgress<TResult> With<TResult>(
+        this IActivableWithoutCancellationTaskWithProgress<TResult> task,
+        Action<IActivableWithoutCancellationTaskWithProgress<TResult>> modifier)
+    {
+        modifier(task);
+        return task;
+    }
+}
