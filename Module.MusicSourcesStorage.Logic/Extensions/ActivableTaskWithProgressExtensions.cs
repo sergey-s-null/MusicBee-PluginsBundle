885b4af161e2d1c060268c1fff6e6d26699406bc
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 20:06:14 +0300

    Add extensions

--- Module.MusicSourcesStorage.Logic/Extensions/ActivableTaskWithProgressExtensions.cs
+++ Module.MusicSourcesStorage.Logic/Extensions/ActivableTaskWithProgressExtensions.cs
@@ -31,4 +31,52 @@ public static class ActivableTaskWithProgressExtensions
         task.Activate(token);
         return task;
     }
+
+    public static IActivableMultiStepTaskWithProgress<TFirstArgs, TResult>
+        Chain<TFirstArgs, TFirstResult, TSecondArgs, TResult>(
+            this IActivableTaskWithProgress<TFirstArgs, TFirstResult> firstTask,
+            Func<TFirstArgs, TFirstResult, TSecondArgs> secondArgsSelector,
+            IActivableTaskWithProgress<TSecondArgs, TResult> secondTask
+        )
+    {
+        return firstTask
+            .AsMultiStep()
+            .Chain(
+                secondArgsSelector,
+                secondTask.AsMultiStep(),
+                (_, _, result) => result
+            );
+    }
+
+    public static IActivableTaskWithProgress<TChangedArgs, TResult>
+        ChangeArgs<TArgs, TResult, TChangedArgs>(
+            this IActivableTaskWithProgress<TArgs, TResult> task,
+            Func<TChangedArgs, TArgs> oldArgsSelector
+        )
+    {
+        return new ActivableTaskWithChangedArgsWrapper<TArgs, TResult, TChangedArgs>(
+            task,
+            oldArgsSelector
+        );
+    }
+
+    public static IActivableTaskWithProgress<TArgs, TChangedResult>
+        ModifyResult<TArgs, TResult, TChangedResult>(
+            this IActivableTaskWithProgress<TArgs, TResult> task,
+            Func<TArgs, TResult, TChangedResult> resultSelector
+        )
+    {
+        return new ActivableTaskWithChangedResultWrapper<TArgs, TResult, TChangedResult>(
+            task,
+            resultSelector
+        );
+    }
+
+    public static IActivableMultiStepTaskWithProgress<TArgs, TResult>
+        AsMultiStep<TArgs, TResult>(
+            this IActivableTaskWithProgress<TArgs, TResult> task
+        )
+    {
+        return new ActivableMultiStepTaskWrapper<TArgs, TResult>(task);
+    }
 }
