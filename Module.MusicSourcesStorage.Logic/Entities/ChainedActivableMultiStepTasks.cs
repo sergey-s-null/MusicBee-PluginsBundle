57ce01dcea6f9ce5d814b29b5d7b769db558dca9
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 19:56:10 +0300

    Add ChainedActivableMultiStepTasks, MultiStepTaskWrapperBase

---
+++ Module.MusicSourcesStorage.Logic/Entities/ChainedActivableMultiStepTasks.cs
@@ -0,0 +1,86 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Entities;
+
+public sealed class ChainedActivableMultiStepTasks<TFirstArgs, TFirstResult, TSecondArgs, TSecondResult, TResult> :
+    MultiStepTaskWrapperBase<TResult>,
+    IActivableMultiStepTaskWithProgress<TFirstArgs, TResult>
+{
+    public override int StepCount => _firstTask.StepCount + _secondTask.StepCount;
+
+    public override bool IsActivated => _isActivated;
+
+    public override Task<TResult> Task => IsActivated && _task is not null
+        ? _task
+        : throw new InvalidOperationException("Task is not started.");
+
+    private bool _isActivated;
+    private Task<TResult>? _task;
+    private readonly IActivableMultiStepTaskWithProgress<TFirstArgs, TFirstResult> _firstTask;
+    private readonly Func<TFirstArgs, TFirstResult, TSecondArgs> _secondArgsSelector;
+    private readonly IActivableMultiStepTaskWithProgress<TSecondArgs, TSecondResult> _secondTask;
+    private readonly Func<TFirstArgs, TFirstResult, TSecondResult, TResult> _resultSelector;
+
+    public ChainedActivableMultiStepTasks(
+        IActivableMultiStepTaskWithProgress<TFirstArgs, TFirstResult> firstTask,
+        Func<TFirstArgs, TFirstResult, TSecondArgs> secondArgsSelector,
+        IActivableMultiStepTaskWithProgress<TSecondArgs, TSecondResult> secondTask,
+        Func<TFirstArgs, TFirstResult, TSecondResult, TResult> resultSelector)
+    {
+        _firstTask = firstTask;
+        _secondArgsSelector = secondArgsSelector;
+        _secondTask = secondTask;
+        _resultSelector = resultSelector;
+
+        ValidateInternalTasksNotActivated();
+    }
+
+    public void Activate(TFirstArgs args, CancellationToken token)
+    {
+        if (IsActivated || _task is not null)
+        {
+            throw new InvalidOperationException("Task already started.");
+        }
+
+        _task = System.Threading.Tasks.Task.Run(
+            () => Execute(args, token),
+            token
+        );
+        _isActivated = true;
+    }
+
+    private TResult Execute(TFirstArgs args, CancellationToken token)
+    {
+        InitializeEvents(_firstTask, 0);
+        _firstTask.Activate(args, token);
+        var firstResult = _firstTask.Task.Result;
+
+        InitializeEventsForFinalTaskWithDifferentResult(
+            _secondTask,
+            secondResult => _resultSelector(args, firstResult, secondResult),
+            _firstTask.StepCount
+        );
+        var secondArgs = _secondArgsSelector(args, firstResult);
+        _secondTask.Activate(secondArgs, token);
+        var secondResult = _secondTask.Task.Result;
+
+        return _resultSelector(args, firstResult, secondResult);
+    }
+
+    private void ValidateInternalTasksNotActivated()
+    {
+        if (_firstTask.IsActivated)
+        {
+            throw new InvalidOperationException(
+                "Could not wrap activated task. (First task is activated)"
+            );
+        }
+
+        if (_secondTask.IsActivated)
+        {
+            throw new InvalidOperationException(
+                "Could not wrap activated task. (Second task is activated)"
+            );
+        }
+    }
+}
---
+++ Module.MusicSourcesStorage.Logic/Entities/MultiStepTaskWrapperBase.cs
@@ -0,0 +1,82 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Entities;
+
+public abstract class MultiStepTaskWrapperBase<TResult> : IMultiStepTaskWithProgress<TResult>
+{
+    public event EventHandler<StepProgressChangedEventArgs>? ProgressChanged;
+    public event EventHandler<StepFailedEventArgs>? Failed;
+    public event EventHandler<StepCancelledEventArgs>? Cancelled;
+    public event EventHandler<StepSuccessfullyCompletedEventArgs>? StepSuccessfullyCompleted;
+    public event EventHandler<TaskResultEventArgs<TResult>>? FullySuccessfullyCompleted;
+
+    public abstract int StepCount { get; }
+
+    public abstract bool IsActivated { get; }
+
+    public abstract Task<TResult> Task { get; }
+
+    protected void InitializeEventsForFinalTask(ITaskWithProgress<TResult> task, int stepIndex)
+    {
+        InitializeCommonEvents(task, stepIndex);
+        task.SuccessfullyCompleted += (_, args) =>
+        {
+            StepSuccessfullyCompleted?.Invoke(
+                this,
+                new StepSuccessfullyCompletedEventArgs(stepIndex)
+            );
+            FullySuccessfullyCompleted?.Invoke(
+                this,
+                new TaskResultEventArgs<TResult>(args.Result)
+            );
+        };
+    }
+
+    protected void InitializeEvents<T>(IMultiStepTaskWithProgress<T> task, int stepOffset)
+    {
+        InitializeCommonEvents(task, stepOffset);
+    }
+
+    protected void InitializeEventsForFinalTaskWithDifferentResult<TDifferentResult>(
+        IMultiStepTaskWithProgress<TDifferentResult> task,
+        Func<TDifferentResult, TResult> resultSelector,
+        int stepOffset)
+    {
+        InitializeCommonEvents(task, stepOffset);
+        task.FullySuccessfullyCompleted += (_, args) => FullySuccessfullyCompleted?.Invoke(
+            this,
+            new TaskResultEventArgs<TResult>(resultSelector(args.Result))
+        );
+    }
+
+    private void InitializeCommonEvents<T>(ITaskWithProgress<T> task, int stepIndex)
+    {
+        task.ProgressChanged += (_, args) =>
+            ProgressChanged?.Invoke(
+                this,
+                new StepProgressChangedEventArgs(stepIndex, args.Progress)
+            );
+        task.Failed += (_, args) => Failed?.Invoke(this, new StepFailedEventArgs(stepIndex, args.Exception));
+        task.Cancelled += (_, _) => Cancelled?.Invoke(this, new StepCancelledEventArgs(stepIndex));
+    }
+
+    private void InitializeCommonEvents<T>(IMultiStepTaskWithProgress<T> task, int stepOffset)
+    {
+        task.ProgressChanged += (_, args) => ProgressChanged?.Invoke(
+            this,
+            new StepProgressChangedEventArgs(args.StepIndex + stepOffset, args.Progress)
+        );
+        task.Failed += (_, args) => Failed?.Invoke(
+            this,
+            new StepFailedEventArgs(args.StepIndex + stepOffset, args.Exception)
+        );
+        task.Cancelled += (_, args) => Cancelled?.Invoke(
+            this,
+            new StepCancelledEventArgs(args.StepIndex + stepOffset)
+        );
+        task.StepSuccessfullyCompleted += (_, args) => StepSuccessfullyCompleted?.Invoke(
+            this,
+            new StepSuccessfullyCompletedEventArgs(args.StepIndex + stepOffset)
+        );
+    }
+}
