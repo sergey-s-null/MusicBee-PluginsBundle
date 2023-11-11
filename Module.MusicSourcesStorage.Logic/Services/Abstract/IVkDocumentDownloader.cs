2e79e4e0012145d2f2c8a823c91f31292c746c0c
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 14:34:46 +0300

    Add VkDocumentDownloader

---
+++ Module.MusicSourcesStorage.Logic/Services/Abstract/IVkDocumentDownloader.cs
@@ -0,0 +1,9 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+public interface IVkDocumentDownloader
+{
+    IActivableTaskWithProgress<VkDocument, string> CreateDownloadTask();
+}
---
+++ Module.MusicSourcesStorage.Logic/Services/VkDocumentDownloader.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using Module.Core.Helpers;
+using Module.MusicSourcesStorage.Core.Entities.Abstract;
+using Module.MusicSourcesStorage.Logic.Delegates;
+using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+using Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Services;
+
+public sealed class VkDocumentDownloader : IVkDocumentDownloader
+{
+    private readonly IModuleConfiguration _configuration;
+
+    public VkDocumentDownloader(IModuleConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IActivableTaskWithProgress<VkDocument, string> CreateDownloadTask()
+    {
+        return new ActivableTaskWithProgress<VkDocument, string>(DownloadInternal);
+    }
+
+    private string DownloadInternal(
+        VkDocument document,
+        RelativeProgressCallback progressCallback,
+        CancellationToken token)
+    {
+        progressCallback(0);
+        var targetFilePath = GetTargetFilePath(document);
+        DirectoryHelper.CreateForFile(targetFilePath);
+        DownloadFile(document.Uri, targetFilePath, progressCallback, token);
+        return targetFilePath;
+    }
+
+    private string GetTargetFilePath(VkDocument document)
+    {
+        var fileName = PathHelper.ReplaceInvalidCharacters(document.Name, "_");
+        return Path.Combine(_configuration.VkDocumentsDownloadingDirectory, fileName);
+    }
+
+    private static void DownloadFile(
+        string sourceUri,
+        string targetFilePath,
+        RelativeProgressCallback progressCallback,
+        CancellationToken token)
+    {
+        using var webClient = new WebClient();
+        token.Register(webClient.CancelAsync);
+        webClient.DownloadProgressChanged += (_, args) => progressCallback(args.ProgressPercentage / 100.0);
+
+        webClient.DownloadFile(sourceUri, targetFilePath);
+    }
+}
