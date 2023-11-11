57e4c311bcaea0375e08e1b9a9fd0d2cf2829d04
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 21:25:55 +0300

    Simplify ActivableTaskWithArgs

--- Module.MusicSourcesStorage.Logic/Entities/ActivableTaskWithArgs.cs
+++ Module.MusicSourcesStorage.Logic/Entities/ActivableTaskWithArgs.cs
@@ -2,16 +2,13 @@
 
 namespace Module.MusicSourcesStorage.Logic.Entities;
 
-public sealed class ActivableTaskWithArgs<TArgs, TResult> : IActivableTaskWithProgress<TResult>
+public sealed class ActivableTaskWithArgs<TArgs, TResult> :
+    TaskWrapperBase<TResult>,
+    IActivableTaskWithProgress<TResult>
 {
-    public event EventHandler<ProgressChangedEventArgs>? ProgressChanged;
-    public event EventHandler<TaskFailedEventArgs>? Failed;
-    public event EventHandler? Cancelled;
-    public event EventHandler<TaskResultEventArgs<TResult>>? SuccessfullyCompleted;
+    public override bool IsActivated => _internalTask.IsActivated;
 
-    public bool IsActivated => _internalTask.IsActivated;
-
-    public Task<TResult> Task => _internalTask.Task;
+    public override Task<TResult> Task => _internalTask.Task;
 
     private readonly IActivableTaskWithProgress<TArgs, TResult> _internalTask;
     private readonly TArgs _args;
@@ -23,19 +20,11 @@ public sealed class ActivableTaskWithArgs<TArgs, TResult> : IActivableTaskWithPr
         _internalTask = internalTask;
         _args = args;
 
-        InitializeEvents();
+        InitializeEvents(_internalTask);
     }
 
     public void Activate(CancellationToken token)
     {
         _internalTask.Activate(_args, token);
-    }
-
-    private void InitializeEvents()
-    {
-        _internalTask.ProgressChanged += ProgressChanged;
-        _internalTask.Failed += Failed;
-        _internalTask.Cancelled += Cancelled;
-        _internalTask.SuccessfullyCompleted += SuccessfullyCompleted;
     }
 }
