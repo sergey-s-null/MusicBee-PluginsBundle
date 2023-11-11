6b622cd46c3aa65db0e7bb74b33cb7dc78dc8f87
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 22:58:07 +0300

    Implement properties in ConnectedDirectoryVM

--- Module.MusicSourcesStorage.Gui/ViewModels/Nodes/ConnectedDirectoryVM.cs
+++ Module.MusicSourcesStorage.Gui/ViewModels/Nodes/ConnectedDirectoryVM.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using System.Windows.Input;
 using Module.MusicSourcesStorage.Gui.AbstractViewModels.Nodes;
+using Module.MusicSourcesStorage.Logic.Enums;
 using Module.Mvvm.Extension;
+using Module.Mvvm.Extension.Helpers;
 using PropertyChanged;
 
 namespace Module.MusicSourcesStorage.Gui.ViewModels.Nodes;
@@ -9,19 +11,22 @@ namespace Module.MusicSourcesStorage.Gui.ViewModels.Nodes;
 [AddINotifyPropertyChangedInterface]
 public sealed class ConnectedDirectoryVM : DirectoryVM, IConnectedDirectoryVM
 {
-    public bool IsProcessing => throw new NotImplementedException();
+    public bool IsProcessing { get; private set; }
 
-    public bool CanDownload => throw new NotImplementedException();
-    public bool IsDownloaded => throw new NotImplementedException();
+    public bool CanDownload { get; private set; }
+    public bool IsDownloaded { get; private set; }
 
-    public bool CanDelete => throw new NotImplementedException();
-    public bool IsDeleted => throw new NotImplementedException();
+    public bool CanDelete { get; private set; }
+    public bool IsDeleted { get; private set; }
 
-    public bool IsListened => throw new NotImplementedException();
+    public bool IsListened { get; private set; }
 
-    public bool HasDownloadedAndNotAttachedToLibraryFiles => throw new NotImplementedException();
+    public bool HasDownloadedAndNotAttachedToLibraryFiles { get; private set; }
 
-    public Stream? CoverStream => throw new NotImplementedException();
+    // todo
+    public Stream? CoverStream => null;
+
+    #region Commands
 
     public ICommand Download => _downloadCmd ??= new RelayCommand(DownloadCmd);
     public ICommand Delete => _deleteCmd ??= new RelayCommand(DeleteCmd);
@@ -33,8 +38,12 @@ public sealed class ConnectedDirectoryVM : DirectoryVM, IConnectedDirectoryVM
     private ICommand? _markAsListenedCmd;
     private ICommand? _markAsNotListenedCmd;
 
+    #endregion
+
     public ConnectedDirectoryVM(string name, IReadOnlyList<INodeVM> childNodes) : base(name, childNodes)
     {
+        RegisterCallbacks();
+        UpdateState();
     }
 
     private void DownloadCmd()
@@ -55,5 +64,146 @@ public sealed class ConnectedDirectoryVM : DirectoryVM, IConnectedDirectoryVM
     private void MarkAllAsNotListenedCmd()
     {
         throw new NotImplementedException();
+    }
+
+    private void RegisterCallbacks()
+    {
+        foreach (var processable in ChildNodes.OfType<IProcessableVM>())
+        {
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                processable,
+                x => x.IsProcessing,
+                (_, _) => UpdateIsProcessing()
+            );
+        }
+
+        foreach (var downloadable in ChildNodes.OfType<IDownloadableVM>())
+        {
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                downloadable,
+                x => x.CanDownload,
+                (_, _) => UpdateCanDownload()
+            );
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                downloadable,
+                x => x.IsDownloaded,
+                (_, _) => UpdateIsDownloaded()
+            );
+        }
+
+        foreach (var deletable in ChildNodes.OfType<IDeletableVM>())
+        {
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                deletable,
+                x => x.CanDelete,
+                (_, _) => UpdateCanDelete()
+            );
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                deletable,
+                x => x.IsDeleted,
+                (_, _) => UpdateIsDeleted()
+            );
+        }
+
+        foreach (var listenable in ChildNodes.OfType<IMarkableAsListenedVM>())
+        {
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                listenable,
+                x => x.IsListened,
+                (_, _) => UpdateIsListened()
+            );
+        }
+
+        foreach (var node in ChildNodes)
+        {
+            switch (node)
+            {
+                case IConnectedMusicFileVM musicFile:
+                    ViewModelHelper.RegisterPropertyChangedCallback(
+                        musicFile,
+                        x => x.Location,
+                        (_, _) => UpdateHasDownloadedAndNotAttachedToLibraryFiles()
+                    );
+                    return;
+                case IConnectedDirectoryVM directory:
+                    ViewModelHelper.RegisterPropertyChangedCallback(
+                        directory,
+                        x => x.HasDownloadedAndNotAttachedToLibraryFiles,
+                        (_, _) => UpdateHasDownloadedAndNotAttachedToLibraryFiles()
+                    );
+                    return;
+            }
+        }
+    }
+
+    private void UpdateState()
+    {
+        UpdateIsProcessing();
+        UpdateCanDownload();
+        UpdateIsDownloaded();
+        UpdateCanDelete();
+        UpdateIsDeleted();
+        UpdateIsListened();
+        UpdateHasDownloadedAndNotAttachedToLibraryFiles();
+    }
+
+    private void UpdateIsProcessing()
+    {
+        IsProcessing = ChildNodes
+            .OfType<IProcessableVM>()
+            .Any(x => IsProcessing);
+    }
+
+    private void UpdateCanDownload()
+    {
+        CanDownload = ChildNodes
+            .OfType<IDownloadableVM>()
+            .Any(x => CanDownload);
+    }
+
+    private void UpdateIsDownloaded()
+    {
+        IsDownloaded = ChildNodes
+            .OfType<IDownloadableVM>()
+            .All(x => IsDownloaded);
+    }
+
+    private void UpdateCanDelete()
+    {
+        CanDelete = ChildNodes
+            .OfType<IDeletableVM>()
+            .Any(x => x.CanDelete);
+    }
+
+    private void UpdateIsDeleted()
+    {
+        IsDeleted = ChildNodes
+            .OfType<IDeletableVM>()
+            .All(x => x.IsDeleted);
+    }
+
+    private void UpdateIsListened()
+    {
+        IsListened = ChildNodes
+            .OfType<IMarkableAsListenedVM>()
+            .All(x => x.IsListened);
+    }
+
+    private void UpdateHasDownloadedAndNotAttachedToLibraryFiles()
+    {
+        foreach (var node in ChildNodes)
+        {
+            switch (node)
+            {
+                case IConnectedMusicFileVM { Location: MusicFileLocation.Incoming }:
+                    HasDownloadedAndNotAttachedToLibraryFiles = true;
+                    return;
+                case IConnectedDirectoryVM { HasDownloadedAndNotAttachedToLibraryFiles: true }:
+                    HasDownloadedAndNotAttachedToLibraryFiles = true;
+                    return;
+            }
+        }
+
+        HasDownloadedAndNotAttachedToLibraryFiles = false;
     }
 }
