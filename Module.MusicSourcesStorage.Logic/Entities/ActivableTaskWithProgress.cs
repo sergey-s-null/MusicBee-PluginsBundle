0094d565af7dff48e407a119724649f89de8656e
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 14:32:33 +0300

    Add ActivableTaskWithProgress, TaskWithProgressBase

---
+++ Module.MusicSourcesStorage.Logic/Entities/ActivableTaskWithProgress.cs
@@ -0,0 +1,89 @@
+﻿using Module.MusicSourcesStorage.Logic.Delegates;
+using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Entities;
+
+public sealed class ActivableTaskWithProgress<TResult> :
+    TaskWithProgressBase<TResult>,
+    IActivableTaskWithProgress<TResult>
+{
+    public override bool IsActivated => _isActivated;
+
+    public override Task<TResult> Task => IsActivated && _task is not null
+        ? _task
+        : throw new InvalidOperationException("Task is not activated.");
+
+    private bool _isActivated;
+    private Task<TResult>? _task;
+    private readonly TaskFunction<TResult> _taskFunction;
+
+    public ActivableTaskWithProgress(Func<CancellationToken, TResult> taskFunction)
+        : this(
+            (progressCallback, internalToken) =>
+            {
+                progressCallback(0);
+                var result = taskFunction(internalToken);
+                progressCallback(1);
+                return result;
+            }
+        )
+    {
+    }
+
+    public ActivableTaskWithProgress(TaskFunction<TResult> taskFunction)
+    {
+        _taskFunction = taskFunction;
+    }
+
+    public void Activate(CancellationToken token)
+    {
+        if (IsActivated || _task is not null)
+        {
+            throw new InvalidOperationException("Task already activated.");
+        }
+
+        _task = System.Threading.Tasks.Task.Run(
+            () => _taskFunction(OnProgressChanged, token),
+            token
+        );
+        // ReSharper disable once MethodSupportsCancellation
+        _task.ContinueWith(OnTaskEnded);
+        _isActivated = true;
+    }
+}
+
+public sealed class ActivableTaskWithProgress<TArgs, TResult> :
+    TaskWithProgressBase<TResult>,
+    IActivableTaskWithProgress<TArgs, TResult>
+{
+    public override bool IsActivated => _isActivated;
+
+    public override Task<TResult> Task => IsActivated && _task is not null
+        ? _task
+        : throw new InvalidOperationException("Task is not activated.");
+
+    private bool _isActivated;
+    private Task<TResult>? _task;
+    private readonly TaskFunction<TArgs, TResult> _taskFunction;
+
+    public ActivableTaskWithProgress(TaskFunction<TArgs, TResult> taskFunction)
+    {
+        _taskFunction = taskFunction;
+    }
+
+    public void Activate(TArgs args, CancellationToken token = default)
+    {
+        if (IsActivated || _task is not null)
+        {
+            throw new InvalidOperationException("Task already activated.");
+        }
+
+        _task = System.Threading.Tasks.Task.Run(
+            () => _taskFunction(args, OnProgressChanged, token),
+            token
+        );
+        // ReSharper disable once MethodSupportsCancellation
+        _task.ContinueWith(OnTaskEnded);
+        _isActivated = true;
+    }
+}
---
+++ Module.MusicSourcesStorage.Logic/Entities/TaskWithProgressBase.cs
@@ -0,0 +1,44 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Entities;
+
+public abstract class TaskWithProgressBase<TResult> : ITaskWithProgress<TResult>
+{
+    public event EventHandler<ProgressChangedEventArgs>? ProgressChanged;
+    public event EventHandler<TaskFailedEventArgs>? Failed;
+    public event EventHandler? Cancelled;
+    public event EventHandler<TaskResultEventArgs<TResult>>? SuccessfullyCompleted;
+
+    public abstract bool IsActivated { get; }
+
+    public abstract Task<TResult> Task { get; }
+
+    protected void OnTaskEnded(Task<TResult> task)
+    {
+        switch (task)
+        {
+            case { Status: TaskStatus.RanToCompletion }:
+                SuccessfullyCompleted?.Invoke(this, new TaskResultEventArgs<TResult>(task.Result));
+                break;
+            case { Status: TaskStatus.Canceled }:
+                Cancelled?.Invoke(this, EventArgs.Empty);
+                break;
+            case { Exception: not null }:
+                Failed?.Invoke(this, new TaskFailedEventArgs(task.Exception));
+                break;
+            default:
+                Failed?.Invoke(this, new TaskFailedEventArgs(new Exception(
+                    $"Task finished without exception with status {task.Status}."
+                )));
+                break;
+        }
+    }
+
+    protected void OnProgressChanged(double progress)
+    {
+        ProgressChanged?.Invoke(
+            this,
+            new ProgressChangedEventArgs(progress)
+        );
+    }
+}
