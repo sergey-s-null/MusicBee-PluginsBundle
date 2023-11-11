d7ac62ecfad4e68ac4d296af123df0eb24856cd7
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 19:57:11 +0300

    Add ActivableMultiStepTaskWithProgressExtensions

---
+++ Module.MusicSourcesStorage.Logic/Extensions/ActivableMultiStepTaskWithProgressExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Extensions;
+
+public static class ActivableMultiStepTaskWithProgressExtensions
+{
+    public static IActivableMultiStepTaskWithProgress<TFirstArgs, TResult>
+        Chain<TFirstArgs, TFirstResult, TSecondArgs, TSecondResult, TResult>(
+            this IActivableMultiStepTaskWithProgress<TFirstArgs, TFirstResult> firstTask,
+            Func<TFirstArgs, TFirstResult, TSecondArgs> secondArgsSelector,
+            IActivableMultiStepTaskWithProgress<TSecondArgs, TSecondResult> secondTask,
+            Func<TFirstArgs, TFirstResult, TSecondResult, TResult> resultSelector
+        )
+    {
+        return new ChainedActivableMultiStepTasks<TFirstArgs, TFirstResult, TSecondArgs, TSecondResult, TResult>(
+            firstTask,
+            secondArgsSelector,
+            secondTask,
+            resultSelector
+        );
+    }
+}
