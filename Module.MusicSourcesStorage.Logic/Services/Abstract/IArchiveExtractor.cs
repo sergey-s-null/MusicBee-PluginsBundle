c971b7b3b3e238a9b509c0c49d3a26cbcbebfbf6
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-11 20:09:15 +0300

    comment

--- Module.MusicSourcesStorage.Logic/Services/Abstract/IArchiveExtractor.cs
+++ Module.MusicSourcesStorage.Logic/Services/Abstract/IArchiveExtractor.cs
@@ -8,5 +8,6 @@ public interface IArchiveExtractor
     /// <summary>
     /// Create task that extract single file from archive to specified file path.
     /// </summary>
+    /// <returns>Task with result as path to extracted file.</returns>
     IActivableTaskWithProgress<FileExtractionArgs, string> CreateFileExtractionTask();
 }
