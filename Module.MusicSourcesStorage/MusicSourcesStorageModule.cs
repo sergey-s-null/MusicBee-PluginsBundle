751a1b4cf988c8a8ada7c2b7f973d8b43e8dc495
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-15 22:20:22 +0300

    Add MusicSourcesStorageModule

---
+++ Module.MusicSourcesStorage/MusicSourcesStorageModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using Module.MusicSourcesStorage.Gui;
+
+namespace Module.MusicSourcesStorage;
+
+public sealed class MusicSourcesStorageModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterModule<DIModule>();
+        builder.RegisterModule<Logic.DIModule>();
+        builder.RegisterModule<Database.DIModule>();
+    }
+}
