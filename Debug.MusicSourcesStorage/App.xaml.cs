9a15eb6a99b8b2c281ecba225a79ce5a93f019ee
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-15 22:24:30 +0300

    Add Debug.MusicSourcesStorage project

---
+++ Debug.MusicSourcesStorage/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Autofac;
+using Module.MusicSourcesStorage.Gui.Views;
+
+namespace Debug.MusicSourcesStorage
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var container = Container.Create();
+            var window = container.Resolve<MusicSourcesWindow>();
+            window.ShowDialog();
+        }
+    }
+}
---
+++ Debug.MusicSourcesStorage/Container.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Module.MusicSourcesStorage;
+
+namespace Debug.MusicSourcesStorage;
+
+public static class Container
+{
+    public static IContainer Create()
+    {
+        var builder = new ContainerBuilder();
+
+        builder.RegisterModule<MusicSourcesStorageModule>();
+
+        return builder.Build();
+    }
+}
