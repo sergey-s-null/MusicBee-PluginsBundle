99771bc667da373b1e7b4f59a9a0d26a357151e6
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-18 15:21:52 +0300

    doc

--- Module.MusicSourcesStorage.Logic/Services/Abstract/IVkDocumentDownloadingTaskManager.cs
+++ Module.MusicSourcesStorage.Logic/Services/Abstract/IVkDocumentDownloadingTaskManager.cs
@@ -5,5 +5,11 @@ namespace Module.MusicSourcesStorage.Logic.Services.Abstract;
 
 public interface IVkDocumentDownloadingTaskManager
 {
+    /// <summary>
+    /// Return task with<br/>
+    /// - Args (<see cref="VkDocument"/>) - document to download;<br/>
+    /// - Result (<see cref="string"/>) - downloaded file path.
+    /// </summary>
+    /// <returns></returns>
     IActivableTask<VkDocument, string> CreateDownloadTask();
 }
