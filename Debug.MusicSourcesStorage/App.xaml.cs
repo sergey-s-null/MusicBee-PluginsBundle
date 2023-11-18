d924962bc2c3c5abbae08afefd82e377c61606e7
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-18 11:52:15 +0300

    -

--- Debug.MusicSourcesStorage/App.xaml.cs
+++ Debug.MusicSourcesStorage/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using Autofac;
+using Module.MusicSourcesStorage.Gui.Services.Abstract;
 using Module.MusicSourcesStorage.Gui.Views;
 
 namespace Debug.MusicSourcesStorage
@@ -12,8 +13,18 @@ namespace Debug.MusicSourcesStorage
         protected override void OnStartup(StartupEventArgs e)
         {
             var container = Container.Create(true);
+            OpenMusicSourcesWindow(container);
+        }
+
+        private static void OpenMusicSourcesWindow(IComponentContext container)
+        {
             var window = container.Resolve<MusicSourcesWindow>();
             window.ShowDialog();
+        }
+
+        private static void AddVkPostWithArchiveSource(IComponentContext container)
+        {
+            container.Resolve<IWizardService>().AddVkPostWithArchiveSource();
         }
     }
 }
