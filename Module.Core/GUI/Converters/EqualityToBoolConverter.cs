3843d67bc0e14cd06733a8610c9c6264019eadf0
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-18 14:26:04 +0300

    Add bindings to Progress component

---
+++ Module.Core/GUI/Converters/EqualityToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Module.Core.GUI.Converters;
+
+public sealed class EqualityToBoolConverter : IValueConverter
+{
+    public bool ValueOnEqual { get; set; }
+
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null || parameter is null)
+        {
+            return !ValueOnEqual;
+        }
+
+        return value.Equals(parameter)
+            ? ValueOnEqual
+            : !ValueOnEqual;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ Module.MusicSourcesStorage.Gui/DesignTimeViewModels/ProgressDTVM.cs
@@ -0,0 +1,11 @@
+﻿using Module.MusicSourcesStorage.Gui.AbstractViewModels;
+
+namespace Module.MusicSourcesStorage.Gui.DesignTimeViewModels;
+
+public sealed class ProgressDTVM : IProgressVM
+{
+    public int StepNumber => 2;
+    public int StepCount => 3;
+
+    public int Percentage => 65;
+}
