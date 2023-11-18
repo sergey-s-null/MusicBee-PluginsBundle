d7ed0896bf9eb6e882cbed0e5cff9299cb33d4af
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-18 14:15:40 +0300

    -

--- Module.Core/GUI/Converters/IsInstanceOfConverter.cs
+++ Module.Core/GUI/Converters/IsInstanceOfConverter.cs
@@ -5,7 +5,7 @@ namespace Module.Core.GUI.Converters;
 
 public sealed class IsInstanceOfConverter : IValueConverter
 {
-    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (parameter is not Type baseType)
         {
@@ -15,7 +15,7 @@ public sealed class IsInstanceOfConverter : IValueConverter
         return baseType.IsInstanceOfType(value);
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
     }
