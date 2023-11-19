eaad20e14c136a376ee8397b309a9dab9207a517
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-19 13:43:13 +0300

    Add bindings to SelectTorrentFileStep

---
+++ Module.MusicSourcesStorage.Gui/AbstractViewModels/WizardSteps/ISelectTorrentFileStepVM.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace Module.MusicSourcesStorage.Gui.AbstractViewModels.WizardSteps;
+
+public interface ISelectTorrentFileStepVM : IManualWizardStepVM
+{
+    string TorrentFilePath { get; set; }
+    string? TorrentFilePathError { get; }
+
+    ICommand ChangeTorrentFilePath { get; }
+}
