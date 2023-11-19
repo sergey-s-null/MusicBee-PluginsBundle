24c4ec29eafda32ca1021c6c66bb66d6ef51425c
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-19 13:22:09 +0300

    Add SelectTorrentFileStep view

---
+++ Module.MusicSourcesStorage.Gui/Views/WizardSteps/SelectTorrentFileStep.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Module.MusicSourcesStorage.Gui.Views.WizardSteps;
+
+public partial class SelectTorrentFileStep : UserControl
+{
+    public SelectTorrentFileStep()
+    {
+        InitializeComponent();
+    }
+}
