48ea6b61e5d1cd7353e0d2da937170325a81b317
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-19 14:16:05 +0300

    Add TorrentIndexer

---
+++ Module.MusicSourcesStorage.Logic/Services/Abstract/ITorrentIndexer.cs
@@ -0,0 +1,14 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Entities.Tasks.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+public interface ITorrentIndexer
+{
+    /// <summary>
+    /// Return task with<br/>
+    /// - Args (<see cref="string"/>) - path to torrent file;<br/>
+    /// - Result (<see cref="IReadOnlyList{T}"/> with <see cref="SourceFile"/>) - indexed files.
+    /// </summary>
+    IActivableTask<string, IReadOnlyList<SourceFile>> CreateIndexingTask();
+}
---
+++ Module.MusicSourcesStorage.Logic/Services/TorrentIndexer.cs
@@ -0,0 +1,60 @@
+﻿using Module.MusicSourcesStorage.Logic.Delegates;
+using Module.MusicSourcesStorage.Logic.Entities;
+using Module.MusicSourcesStorage.Logic.Entities.Tasks.Abstract;
+using Module.MusicSourcesStorage.Logic.Enums;
+using Module.MusicSourcesStorage.Logic.Factories;
+using Module.MusicSourcesStorage.Logic.Services.Abstract;
+using MonoTorrent;
+
+namespace Module.MusicSourcesStorage.Logic.Services;
+
+public sealed class TorrentIndexer : ITorrentIndexer
+{
+    private readonly IFileClassifier _fileClassifier;
+
+    public TorrentIndexer(IFileClassifier fileClassifier)
+    {
+        _fileClassifier = fileClassifier;
+    }
+
+    public IActivableTask<string, IReadOnlyList<SourceFile>> CreateIndexingTask()
+    {
+        return ActivableTaskFactory.Create<string, IReadOnlyList<SourceFile>>(Index);
+    }
+
+    private IReadOnlyList<SourceFile> Index(
+        string torrentFilePath,
+        RelativeProgressCallback? progressCallback,
+        CancellationToken token)
+    {
+        var torrent = Torrent.Load(torrentFilePath);
+
+        var result = new List<SourceFile>(torrent.Files.Count);
+        progressCallback?.Invoke(0);
+        for (var i = 0; i < torrent.Files.Count; i++)
+        {
+            token.ThrowIfCancellationRequested();
+            result.Add(CreateSourceFile(torrent.Files[i]));
+            progressCallback?.Invoke((double)(i + 1) / torrent.Files.Count);
+        }
+
+        return result;
+    }
+
+    private SourceFile CreateSourceFile(ITorrentFile torrentFile)
+    {
+        var fileType = _fileClassifier.Classify(torrentFile.Path);
+        return CreateSourceFile(fileType, torrentFile);
+    }
+
+    private static SourceFile CreateSourceFile(FileType fileType, ITorrentFile torrentFile)
+    {
+        return fileType switch
+        {
+            FileType.MusicFile => MusicFile.New(torrentFile.Path, torrentFile.Length),
+            FileType.Image => ImageFile.New(torrentFile.Path, torrentFile.Length),
+            FileType.Unknown => UnknownFile.New(torrentFile.Path, torrentFile.Length),
+            _ => throw new ArgumentOutOfRangeException(nameof(fileType), fileType, "Unsupported file type.")
+        };
+    }
+}
