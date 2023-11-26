3db364975e67b1a550794654338b09c2bf650a72
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-26 12:08:30 +0300

    Add settings base structure in DB

---
+++ Module.Settings.Database/Models/Int32SettingValue.cs
@@ -0,0 +1,6 @@
+﻿namespace Module.Settings.Database.Models;
+
+public sealed class Int32SettingValue : SettingValue
+{
+    public int? Value { get; set; }
+}
---
+++ Module.Settings.Database/SettingsContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using Module.Settings.Database.Models;
+
+namespace Module.Settings.Database;
+
+public sealed class SettingsContext : DbContext
+{
+    public DbSet<SettingEntry> Settings { get; set; } = null!;
+
+    public SettingsContext(string connectionString) : base(connectionString)
+    {
+    }
+}
