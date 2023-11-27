a6a6add06ad1307a2d566e91856041b74d8dfca1
Sergey Semenov <sergey.s.null@gmail.com>
2023-11-27 19:21:00 +0300

    Add bindings

---
+++ Module.MusicSourcesStorage.Gui/AbstractViewModels/IMusicSourcesStorageSettingsVM.cs
@@ -0,0 +1,16 @@
+﻿using Mead.MusicBee.Enums;
+using Module.Settings.Gui.AbstractViewModels;
+
+namespace Module.MusicSourcesStorage.Gui.AbstractViewModels;
+
+public interface IMusicSourcesStorageSettingsVM : IBaseSettingsVM
+{
+    string VkDocumentsDownloadingDirectory { get; set; }
+    string SourceFilesDownloadingDirectory { get; set; }
+
+    string CoverSize { get; set; }
+    string? CoverSizeError { get; }
+
+    IReadOnlyList<MetaDataType> AvailableFieldsForFileId { get; }
+    MetaDataType SelectedFileIdField { get; set; }
+}
