b795c474b9d2204f9842079df497d26dd8f1777e
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-01 23:38:23 +0300

    Update doc string

--- Module.MusicSourcesStorage.Logic/Services/Abstract/IVkService.cs
+++ Module.MusicSourcesStorage.Logic/Services/Abstract/IVkService.cs
@@ -5,9 +5,8 @@ namespace Module.MusicSourcesStorage.Logic.Services.Abstract;
 
 public interface IVkService
 {
-    /// <exception cref="VkServiceException">
-    /// Error on get documents
-    /// </exception>
+    /// <exception cref="VkServiceException">Error on get documents</exception>
+    /// <exception cref="NotAuthorizedException">Vk Api is not authorized.</exception>
     Task<IReadOnlyList<VkDocument>> GetAttachedDocumentsFromPostAsync(
         long postOwnerId,
         long postId,
