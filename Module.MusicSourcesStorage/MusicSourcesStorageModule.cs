271a920127571429cbdf4f0855067542cfc1c8b0
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-01 22:49:00 +0300

    Add module actions

--- Module.MusicSourcesStorage/MusicSourcesStorageModule.cs
+++ Module.MusicSourcesStorage/MusicSourcesStorageModule.cs
@@ -1,4 +1,6 @@
 ﻿using Autofac;
+using Module.MusicSourcesStorage.Services;
+using Module.MusicSourcesStorage.Services.Abstract;
 using GuiDIModule = Module.MusicSourcesStorage.Gui.DIModule;
 using LogicDIModule = Module.MusicSourcesStorage.Logic.DIModule;
 using DatabaseDIModule = Module.MusicSourcesStorage.Database.DIModule;
@@ -12,5 +14,10 @@ public sealed class MusicSourcesStorageModule : Autofac.Module
         builder.RegisterModule<GuiDIModule>();
         builder.RegisterModule<LogicDIModule>();
         builder.RegisterModule<DatabaseDIModule>();
+
+        builder
+            .RegisterType<MusicSourcesStorageModuleActions>()
+            .As<IMusicSourcesStorageModuleActions>()
+            .SingleInstance();
     }
 }
---
+++ Module.MusicSourcesStorage/Services/MusicSourcesStorageModuleActions.cs
@@ -0,0 +1,29 @@
+﻿using Module.MusicSourcesStorage.Gui.Services.Abstract;
+using Module.MusicSourcesStorage.Gui.Views;
+using Module.MusicSourcesStorage.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Services;
+
+public sealed class MusicSourcesStorageModuleActions : IMusicSourcesStorageModuleActions
+{
+    private readonly Func<MusicSourcesWindow> _musicSourcesWindowsFactory;
+    private readonly IWizardService _wizardService;
+
+    public MusicSourcesStorageModuleActions(
+        Func<MusicSourcesWindow> musicSourcesWindowsFactory,
+        IWizardService wizardService)
+    {
+        _musicSourcesWindowsFactory = musicSourcesWindowsFactory;
+        _wizardService = wizardService;
+    }
+
+    public void ShowMusicSources()
+    {
+        _musicSourcesWindowsFactory().ShowDialog();
+    }
+
+    public void AddVkPostWithArchiveSource()
+    {
+        _wizardService.AddVkPostWithArchiveSource();
+    }
+}
