51d234dbde6d628250b3c8348109f61da76fbb6e
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-02 14:28:20 +0300

    -todo

--- Debug.MusicSourcesStorage/DebugModuleConfiguration.cs
+++ Debug.MusicSourcesStorage/DebugModuleConfiguration.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.IO;
-using Mead.MusicBee.Enums;
 using Module.MusicSourcesStorage.Core;
 
 namespace Debug.MusicSourcesStorage;
 
-// todo remove useless values
 public sealed class DebugModuleConfiguration : IModuleConfiguration
 {
     private static readonly string DebugFolder = Path.Combine(
@@ -15,17 +13,9 @@ public sealed class DebugModuleConfiguration : IModuleConfiguration
 
     private static readonly string DatabaseFilePath = Path.Combine(DebugFolder, "MusicSourcesStorage.mdf");
 
-    public string VkDocumentsDownloadingDirectory { get; } = Path.Combine(DebugFolder, "VkDocuments");
-
-    public string SourceFilesDownloadingDirectory { get; } = Path.Combine(DebugFolder, "SourceFiles");
-
     public string DatabaseConnectionString { get; } = "Data Source=(LocalDb)\\MSSQLLocalDB;" +
                                                       "Initial Catalog=MusicSourcesStorage;" +
                                                       $"AttachDBFilename={DatabaseFilePath};";
 
     public string SettingsArea => "music-sources-storage";
-
-    public int CoverSize => 100;
-
-    public MetaDataType FileIdField => MetaDataType.Custom10;
 }
