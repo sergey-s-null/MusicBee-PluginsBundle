6224dec25687e53f321f2594bf82405c7aa06634
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-02 16:50:51 +0300

    Add Debug.Settings project

---
+++ Debug.Settings/DebugModuleConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Module.Settings.Core;
+
+namespace Debug.Settings;
+
+public sealed class DebugModuleConfiguration : IModuleConfiguration
+{
+    private static readonly string DebugFolder = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+        "DebugSettings"
+    );
+
+    private static readonly string DatabaseFilePath = Path.Combine(DebugFolder, "Settings.mdf");
+
+    public string DatabaseConnectionString { get; } = "Data Source=(LocalDb)\\MSSQLLocalDB;" +
+                                                      "Initial Catalog=Settings;" +
+                                                      $"AttachDBFilename={DatabaseFilePath};";
+}
