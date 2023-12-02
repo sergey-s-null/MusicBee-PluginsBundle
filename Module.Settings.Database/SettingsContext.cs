33f2b78dbf73e097b805678b7f8cf9f0fe14cee9
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-02 21:59:07 +0300

    Create initial migration for Settings module

--- Module.Settings.Database/SettingsContext.cs
+++ Module.Settings.Database/SettingsContext.cs
@@ -8,6 +8,12 @@ public sealed class SettingsContext : DbContext
 {
     public DbSet<SettingEntry> Settings { get; set; } = null!;
 
+    // Used for ef6 util
+    // ReSharper disable once UnusedMember.Global
+    public SettingsContext()
+    {
+    }
+
     public SettingsContext(IModuleConfiguration configuration)
         : base(configuration.DatabaseConnectionString)
     {
