7b36b1a95ef557f8bde4abbcc1a07495fc1c0f24
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-02 22:02:31 +0300

    Add initial migration to MusicSourcesStorage module

--- Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Database/MusicSourcesStorageContext.cs
+++ Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Database/MusicSourcesStorageContext.cs
@@ -9,6 +9,12 @@ public sealed class MusicSourcesStorageContext : DbContext
     public DbSet<MusicSourceModel> Sources { get; set; } = null!;
     public DbSet<FileModel> Files { get; set; } = null!;
 
+    // Used for ef6 migrations
+    // ReSharper disable once UnusedMember.Global
+    public MusicSourcesStorageContext()
+    {
+    }
+
     public MusicSourcesStorageContext(IModuleConfiguration configuration)
         : base(configuration.DatabaseConnectionString)
     {
