73467fe90659481979ce5fbbc3ad894a7bfb444e
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-03 17:50:18 +0300

    Add gui di module

---
+++ Modules/Module.Vk.Gui/DIModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Module.Vk.GUI.AbstractViewModels;
+using Module.Vk.GUI.ViewModels;
+
+namespace Module.Vk.Gui;
+
+public sealed class DIModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<VkSettingsVM>()
+            .As<IVkSettingsVM>();
+    }
+}
