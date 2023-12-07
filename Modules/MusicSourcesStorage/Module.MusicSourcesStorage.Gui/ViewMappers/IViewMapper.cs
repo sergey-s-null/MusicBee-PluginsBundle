5a287f2599027cc647ae361b6f89658d45b0eb1f
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-07 22:43:57 +0300

    Remove ViewMappers; Add template selector for wizard steps.

--- Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Gui/ViewMappers/IViewMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Module.MusicSourcesStorage.Gui.ViewMappers;
-
-// todo use DataTemplateSelector instead
-public interface IViewMapper
-{
-    bool TryGetViewFactory(Type viewModelType, out Func<FrameworkElement> viewFactory);
-}
