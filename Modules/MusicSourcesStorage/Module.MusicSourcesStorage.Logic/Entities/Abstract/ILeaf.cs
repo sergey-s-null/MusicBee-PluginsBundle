6fea3324a4a25a80db68e99c35115e9df837b88f
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-15 23:34:37 +0300

    Add abstractions for Leaf and Node

---
+++ Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Logic/Entities/Abstract/ILeaf.cs
@@ -0,0 +1,7 @@
+﻿namespace Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+public interface ILeaf<out TValue, out TPathElement>
+{
+    IReadOnlyList<TPathElement> Path { get; }
+    TValue Value { get; }
+}
--- Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Logic/Entities/Leaf.cs
+++ Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Logic/Entities/Leaf.cs
@@ -1,6 +1,8 @@
-﻿namespace Module.MusicSourcesStorage.Logic.Entities;
+﻿using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Entities;
 
 public sealed record Leaf<TValue, TPathElement>(
     IReadOnlyList<TPathElement> Path,
     TValue Value
-);
+) : ILeaf<TValue, TPathElement>;
