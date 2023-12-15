632592a73933a5a623ebf28a261ff7372a1d315a
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-15 23:35:54 +0300

    Add LeavesSeparator

---
+++ Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Logic/Services/Abstract/ILeavesSeparator.cs
@@ -0,0 +1,13 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+public interface ILeavesSeparator<TValue, TPathElement>
+{
+    void SeparateLeavesAtLevel(
+        IReadOnlyList<ILeaf<TValue, TPathElement>> rawLeaves,
+        int level,
+        out IReadOnlyList<ILeaf<TValue, TPathElement>> notLeaves,
+        out IReadOnlyList<ILeaf<TValue, TPathElement>> leaves
+    );
+}
---
+++ Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Logic/Services/LeavesSeparator.cs
@@ -0,0 +1,37 @@
+﻿using Module.MusicSourcesStorage.Logic.Entities.Abstract;
+using Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Services;
+
+public sealed class LeavesSeparator<TValue, TPathElement> : ILeavesSeparator<TValue, TPathElement>
+{
+    public void SeparateLeavesAtLevel(
+        IReadOnlyList<ILeaf<TValue, TPathElement>> rawLeaves,
+        int level,
+        out IReadOnlyList<ILeaf<TValue, TPathElement>> notLeaves,
+        out IReadOnlyList<ILeaf<TValue, TPathElement>> leaves)
+    {
+        var notLeavesInternal = new List<ILeaf<TValue, TPathElement>>();
+        var leavesInternal = new List<ILeaf<TValue, TPathElement>>();
+
+        foreach (var rawLeaf in rawLeaves)
+        {
+            if (IsLeafAtLevel(rawLeaf, level))
+            {
+                leavesInternal.Add(rawLeaf);
+            }
+            else
+            {
+                notLeavesInternal.Add(rawLeaf);
+            }
+        }
+
+        notLeaves = notLeavesInternal;
+        leaves = leavesInternal;
+    }
+
+    private static bool IsLeafAtLevel(ILeaf<TValue, TPathElement> leaf, int level)
+    {
+        return level + 1 == leaf.Path.Count;
+    }
+}
