1d52fedc79b6fc9711d693a81ac5bcc54e560ad1
Sergey Semenov <sergey.s.null@gmail.com>
2023-12-15 23:38:28 +0300

    Add LazyHierarchyBuilderFactory

---
+++ Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Logic/Factories/LazyHierarchyBuilderFactory.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Module.MusicSourcesStorage.Logic.Factories.Abstract;
+using Module.MusicSourcesStorage.Logic.Services;
+using Module.MusicSourcesStorage.Logic.Services.Abstract;
+
+namespace Module.MusicSourcesStorage.Logic.Factories;
+
+public sealed class LazyHierarchyBuilderFactory : IHierarchyBuilderFactory
+{
+    private readonly ILifetimeScope _lifetimeScope;
+
+    public LazyHierarchyBuilderFactory(ILifetimeScope lifetimeScope)
+    {
+        _lifetimeScope = lifetimeScope;
+    }
+
+    public IHierarchyBuilder<TValue, TPathElement> Create<TValue, TPathElement>(
+        Func<TValue, IReadOnlyList<TPathElement>> pathElementsFactory,
+        IEqualityComparer<TPathElement> pathElementEqualityComparer)
+    {
+        return new HierarchyBuilder<TValue, TPathElement>(
+            pathElementsFactory,
+            new LazyLeavesGroupingService<TValue, TPathElement>(
+                _lifetimeScope.Resolve<ILeavesSeparator<TValue, TPathElement>>(),
+                pathElementEqualityComparer
+            )
+        );
+    }
+}
