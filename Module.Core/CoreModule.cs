66bdc4aa00e983940ca6fca4b0f478374890d2dd
Sergey Semenov <sergey.s.null@gmail.com>
2024-02-24 12:42:40 +0300

    Add CoreModule

---
+++ Module.Core/CoreModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Module.Core.Services;
+using Module.Core.Services.Abstract;
+
+namespace Module.Core;
+
+public sealed class CoreModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<SettingsFiles>()
+            .As<ISettingsFiles>()
+            .SingleInstance();
+    }
+}
