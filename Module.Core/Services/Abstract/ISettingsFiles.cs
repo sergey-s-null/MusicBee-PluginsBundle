a4722fc4778ab0a6b033332021b2c2f7053ecd69
Sergey Semenov <sergey.s.null@gmail.com>
2024-02-24 12:32:32 +0300

    Add SettingsFiles

---
+++ Module.Core/Services/Abstract/ISettingsFiles.cs
@@ -0,0 +1,9 @@
+﻿namespace Module.Core.Services.Abstract;
+
+public interface ISettingsFiles
+{
+    string ArtworksSearcherSettingsFilePath { get; }
+    string PlaylistExporterSettingsFilePath { get; }
+    string VkSettingsFilePath { get; }
+    string AudioDownloaderSettingsFilePath { get; }
+}
