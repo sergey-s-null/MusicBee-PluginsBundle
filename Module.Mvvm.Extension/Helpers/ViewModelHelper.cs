2889102223ea71d2678e98158fd389ee1513a1b1
Sergey Semenov <sergey.s.null@gmail.com>
2024-03-11 00:12:07 +0300

    Add ViewModelHelperTests; Refactor RegisterPropertyChangedCallback.

--- Module.Mvvm.Extension/Helpers/ViewModelHelper.cs
+++ Module.Mvvm.Extension/Helpers/ViewModelHelper.cs
@@ -1,32 +1,47 @@
 ﻿using System.ComponentModel;
 using System.Linq.Expressions;
-using Module.Mvvm.Extension.Exceptions;
 
 namespace Module.Mvvm.Extension.Helpers;
 
+// todo rename to NotifyPropertyChangedHelper
 public static class ViewModelHelper
 {
+    // todo rename to "Handler"
     public static void RegisterPropertyChangedCallback<TViewModel, TProperty>(
         TViewModel viewModel,
         Expression<Func<TViewModel, TProperty>> propertySelector,
         Action<TViewModel, TProperty> callback)
     {
+        RegisterPropertyChangedHandler(viewModel, propertySelector, callback, out _);
+    }
+
+    public static void RegisterPropertyChangedHandler<TViewModel, TProperty>(
+        TViewModel viewModel,
+        Expression<Func<TViewModel, TProperty>> propertySelector,
+        Action<TViewModel, TProperty> handler,
+        out Action unregisterHandler)
+    {
         if (viewModel is not INotifyPropertyChanged notifier)
         {
-            throw new CallbackRegistrationException(
+            throw new ArgumentException(
                 $"View model {viewModel} does not implement {nameof(INotifyPropertyChanged)}."
             );
         }
 
         var propertyName = GetPropertyName(propertySelector);
         var selector = propertySelector.Compile();
-        notifier.PropertyChanged += (_, args) =>
+
+        PropertyChangedEventHandler wrappedHandler = (_, args) =>
         {
             if (args.PropertyName == propertyName)
             {
-                callback(viewModel, selector(viewModel));
+                handler(viewModel, selector(viewModel));
             }
         };
+
+        notifier.PropertyChanged += wrappedHandler;
+
+        unregisterHandler = () => notifier.PropertyChanged -= wrappedHandler;
     }
 
     private static string GetPropertyName<TViewModel, TProperty>(
@@ -40,8 +55,17 @@ public static class ViewModelHelper
 
         if (body is not MemberExpression memberExpression)
         {
-            throw new CallbackRegistrationException(
-                "Could not get property name. Body is not a member expression."
+            throw new ArgumentException(
+                "Could not get property name. Body is not a member expression.",
+                nameof(propertySelector)
+            );
+        }
+
+        if (memberExpression.Expression is not ParameterExpression)
+        {
+            throw new ArgumentException(
+                "Property selector is invalid.",
+                nameof(propertySelector)
             );
         }
 
---
+++ Modules/Module.Mvvm.Extension.Tests/ViewModelHelperTests.cs
@@ -0,0 +1,115 @@
+using Module.Mvvm.Extension.Helpers;
+using PropertyChanged;
+
+namespace Module.Mvvm.Extension.Tests;
+
+public sealed class ViewModelHelperTests
+{
+    [Test]
+    public void PropertyChangedHandlerCalled()
+    {
+        var vm = new ThirdVM
+        {
+            Value = 1
+        };
+
+        ThirdVM? actualViewModel = null;
+        var handlerCallCount = 0;
+        var changedValue = 0;
+        ViewModelHelper.RegisterPropertyChangedCallback(
+            vm,
+            x => x.Value,
+            (viewModel, value) =>
+            {
+                actualViewModel = viewModel;
+                handlerCallCount++;
+                changedValue = value;
+            }
+        );
+
+        vm.Value = 42;
+        Assert.Multiple(() =>
+        {
+            Assert.That(handlerCallCount, Is.EqualTo(1));
+            Assert.That(changedValue, Is.EqualTo(42));
+            Assert.That(actualViewModel, Is.Not.Null);
+        });
+        Assert.That(actualViewModel, Is.EqualTo(vm));
+    }
+
+    [Test]
+    public void PropertyChangedHandlerNotCalledAfterUnregister()
+    {
+        var vm = new ThirdVM
+        {
+            Value = 1
+        };
+
+        var handlerCallCount = 0;
+        ViewModelHelper.RegisterPropertyChangedHandler(
+            vm,
+            x => x.Value,
+            (_, _) => handlerCallCount++,
+            out var unregisterHandler
+        );
+
+        vm.Value = 42;
+        Assert.That(handlerCallCount, Is.EqualTo(1));
+
+        unregisterHandler();
+
+        vm.Value = 100;
+        Assert.That(handlerCallCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void ExceptionThrownOnValueInPropertySelector()
+    {
+        var vm = new FirstVM();
+        Assert.Throws<ArgumentException>(() =>
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                vm, x => 42, (_, _) => { }
+            )
+        );
+    }
+
+    [Test]
+    public void ExceptionThrownOnViewModelInPropertySelector()
+    {
+        var vm = new FirstVM();
+        Assert.Throws<ArgumentException>(() =>
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                vm, x => x, (_, _) => { }
+            )
+        );
+    }
+
+    [Test]
+    public void ExceptionThrownOnComplexPropertySelector()
+    {
+        var vm = new FirstVM();
+        Assert.Throws<ArgumentException>(() =>
+            ViewModelHelper.RegisterPropertyChangedCallback(
+                vm, x => x.Second!.Third, (_, _) => { }
+            )
+        );
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    private sealed class FirstVM
+    {
+        public SecondVM? Second { get; set; }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    private sealed class SecondVM
+    {
+        public ThirdVM? Third { get; set; }
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    private sealed class ThirdVM
+    {
+        public int Value { get; set; }
+    }
+}
