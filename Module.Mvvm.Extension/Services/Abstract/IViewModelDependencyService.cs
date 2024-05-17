c9844bd3c8d1bef0a949f8dc1f3f89df6164f575
Sergey Semenov <sergey.s.null@gmail.com>
2024-05-17 21:37:58 +0300

    Add IViewModelDependencyService

---
+++ Module.Mvvm.Extension/Services/Abstract/IViewModelDependencyService.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Module.Mvvm.Extension.Services.Abstract;
+
+public interface IViewModelDependencyService
+{
+    void RegisterDependency<TDependent, TDependentProperty, TDependency, TDependencyProperty>(
+        TDependent dependentViewModel,
+        Expression<Func<TDependent, TDependentProperty>> dependentProperty,
+        TDependency dependencyViewModel,
+        Expression<Func<TDependency, TDependencyProperty>> dependencyProperty,
+        out Action unregisterDependency
+    );
+}
