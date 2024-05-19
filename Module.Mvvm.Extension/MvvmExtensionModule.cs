6ff73baf277a4f89a22551a2363ac07353aabcae
Sergey Semenov <sergey.s.null@gmail.com>
2024-05-19 17:02:31 +0300

    Add MvvmExtensionModule

---
+++ Module.Mvvm.Extension/MvvmExtensionModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Module.Mvvm.Extension.Services;
+using Module.Mvvm.Extension.Services.Abstract;
+
+namespace Module.Mvvm.Extension;
+
+public sealed class MvvmExtensionModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder
+            .RegisterType<ViewModelDependencyService>()
+            .As<IViewModelDependencyService>()
+            .SingleInstance();
+    }
+}
