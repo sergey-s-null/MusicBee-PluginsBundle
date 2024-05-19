b932d2504923c87d03232dccb64e662a55001782
Sergey Semenov <sergey.s.null@gmail.com>
2024-05-19 17:00:42 +0300

    Add NotifyPropertyChangedHelperTests

---
+++ Modules/Module.Mvvm.Extension.Tests/ViewModels/DerivedVM.cs
@@ -0,0 +1,9 @@
+﻿using PropertyChanged;
+
+namespace Module.Mvvm.Extension.Tests.ViewModels;
+
+[AddINotifyPropertyChangedInterface]
+public sealed class DerivedVM : BaseVM
+{
+    public string Text { get; set; } = string.Empty;
+}
