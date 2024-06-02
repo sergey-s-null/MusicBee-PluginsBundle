a8a5919b204c08b64a9a16144592c4e2914fe68d
Sergey Semenov <sergey.s.null@gmail.com>
2024-06-02 23:52:39 +0300

    Rework DeleteFileCommand

--- Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Gui/Commands/DeleteFileCommand.cs
+++ Modules/MusicSourcesStorage/Module.MusicSourcesStorage.Gui/Commands/DeleteFileCommand.cs
@@ -5,62 +5,75 @@ using Module.MusicSourcesStorage.Gui.Exceptions;
 using Module.MusicSourcesStorage.Gui.Helpers;
 using Module.MusicSourcesStorage.Gui.Services.Abstract;
 using Module.MusicSourcesStorage.Logic.Services.Abstract;
+using PropertyChanged;
 
 namespace Module.MusicSourcesStorage.Gui.Commands;
 
+[AddINotifyPropertyChangedInterface]
 public sealed class DeleteFileCommand : ICommand
 {
-    public event EventHandler? CanExecuteChanged;
-
     private readonly int _fileId;
     private readonly string _filePath;
     private readonly bool _askBeforeDelete;
-    private readonly IFilesLocatingService _filesLocatingService;
     private readonly IFileOperationLocker _fileOperationLocker;
     private readonly IFilesDeletingService _filesDeletingService;
+
+    public delegate DeleteFileCommand Factory(int fileId, string filePath, bool askBeforeDelete);
 
     public DeleteFileCommand(
         int fileId,
         string filePath,
         bool askBeforeDelete,
-        IFilesLocatingService filesLocatingService,
         IFileOperationLocker fileOperationLocker,
         IFilesDeletingService filesDeletingService)
     {
         _fileId = fileId;
         _filePath = filePath;
         _askBeforeDelete = askBeforeDelete;
-        _filesLocatingService = filesLocatingService;
         _fileOperationLocker = fileOperationLocker;
         _filesDeletingService = filesDeletingService;
 
         _fileOperationLocker.LockStateChanged += OnLockStateChanged;
     }
 
+    public event EventHandler? CanExecuteChanged;
+    public event EventHandler? Deleted;
+
+    public bool IsProcessing { get; private set; }
+
     public bool CanExecute(object parameter)
     {
-        // TODO subscribe to existence event
-        return !_fileOperationLocker.IsLocked(_fileId)
-               && _filesLocatingService.IsFileExistsAsync(_fileId).Result;
+        return !_fileOperationLocker.IsLocked(_fileId);
     }
 
-    public void Execute(object parameter)
+    public async void Execute(object parameter)
     {
         try
         {
             using var _ = _fileOperationLocker.Lock(_fileId, TimeSpan.Zero);
 
+            IsProcessing = true;
+
             if (_askBeforeDelete
                 && MessageBoxHelper.AskForDeletion(_fileId, _filePath) != MessageBoxResult.Yes)
             {
+                IsProcessing = false;
                 return;
             }
 
-            _filesDeletingService.DeleteAsync(_fileId).Wait();
+            await _filesDeletingService.DeleteAsync(_fileId);
+
+            IsProcessing = false;
+            RaiseDeleted();
         }
         catch (LockTimeoutException)
         {
         }
+    }
+
+    private void RaiseDeleted()
+    {
+        Deleted?.Invoke(this, EventArgs.Empty);
     }
 
     private void OnLockStateChanged(object _, LockStateChangedEventArgs args)
