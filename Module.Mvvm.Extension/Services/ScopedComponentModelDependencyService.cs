c6b17197194bac1f315877113ba6ddb88152b2d3
Sergey Semenov <sergey.s.null@gmail.com>
2024-06-08 13:11:17 +0300

    Add scoped dependency service

---
+++ Module.Mvvm.Extension/Services/ScopedComponentModelDependencyService.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+using Module.Mvvm.Extension.Services.Abstract;
+
+namespace Module.Mvvm.Extension.Services;
+
+public sealed class ScopedComponentModelDependencyService<T>
+    : IScopedComponentModelDependencyService<T>
+{
+    private readonly T _object;
+    private readonly IComponentModelDependencyService _componentModelDependencyService;
+
+    public delegate ScopedComponentModelDependencyService<T> Factory(T @object);
+
+    public ScopedComponentModelDependencyService(
+        T @object,
+        IComponentModelDependencyService componentModelDependencyService)
+    {
+        _object = @object;
+        _componentModelDependencyService = componentModelDependencyService;
+    }
+
+    public void RegisterDependency<TDependentProperty, TDependencyProperty>(
+        Expression<Func<T, TDependentProperty>> dependentProperty,
+        Expression<Func<T, TDependencyProperty>> dependencyProperty,
+        out Action unregisterDependency)
+    {
+        _componentModelDependencyService.RegisterDependency(
+            _object,
+            dependentProperty,
+            _object,
+            dependencyProperty,
+            out unregisterDependency
+        );
+    }
+
+    public void RegisterDependency<TDependentProperty, TDependency, TDependencyProperty>(
+        Expression<Func<T, TDependentProperty>> dependentProperty,
+        TDependency dependencyObject,
+        Expression<Func<TDependency, TDependencyProperty>> dependencyProperty,
+        out Action unregisterDependency)
+    {
+        _componentModelDependencyService.RegisterDependency(
+            _object,
+            dependentProperty,
+            dependencyObject,
+            dependencyProperty,
+            out unregisterDependency
+        );
+    }
+}
